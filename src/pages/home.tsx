6ca57cfa050c194e9d056d041d3235a8a1ce41f7
Zaydek <zaydekdotcom@gmail.com>
2023-01-10 04:15:49 -0800

    Seemingly complete parameterization

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,32 +10,30 @@ import { iota } from "../lib/iota"
 
 export function Home() {
 	return <>
-		<div className="h-320 bg-$trim-color"></div>
-		<div className="-mx-64 sticky t-32">
-			<div className="h-$backdrop-height rounded-b-50% bg-$trim-color"></div>
+		<div className="h-$hero-height bg-$trim-color"></div>
+		<div className="-mx-64 sticky t-$inset-y">
+			<div className="h-$ribbon-height rounded-b-50% bg-$trim-color"></div>
 		</div>
-		{/* Use negative margin-top here because of foreground obscure effect */}
-		<div className="mt-calc(-1_*_($backdrop-height_+_32px)) pb-64 px-$inset-x bg-$gray-color">
+		<div className="mt-calc(-1_*_($ribbon-height_+_$rounding)) pb-calc($inset-y_*_2) px-$inset-x bg-$gray-color">
 			<div className="mx-calc(-1_*_$inset-x) sticky t-0 z-10 flex [&_>_:nth-child(3)]:grow-1">
-				<div className="h-64 w-$inset-x bg-$trim-color"></div>
+				<div className="h-calc($inset-y_+_$rounding) w-$inset-x bg-$trim-color"></div>
 				<div className="relative">
-					<div className="h-64 w-32 bg-$trim-color"></div>
-					<div className="absolute br-0 h-32 w-32 rounded-tl-1e3 bg-$base-color"></div>
+					<div className="h-calc($inset-y_+_$rounding) w-$rounding bg-$trim-color"></div>
+					<div className="absolute br-0 h-$rounding w-$rounding rounded-tl-1e3 bg-$base-color"></div>
 				</div>
-				<div className="h-32 bg-$trim-color"></div>
+				<div className="h-$inset-y bg-$trim-color"></div>
 				<div className="relative">
-					<div className="h-64 w-32 bg-$trim-color"></div>
-					<div className="absolute bl-0 h-32 w-32 rounded-tr-1e3 bg-$gray-color"></div>
+					<div className="h-calc($inset-y_+_$rounding) w-$rounding bg-$trim-color"></div>
+					<div className="absolute bl-0 h-$rounding w-$rounding rounded-tr-1e3 bg-$gray-color"></div>
 				</div>
-				<div className="h-64 w-$inset-x bg-$trim-color"></div>
+				<div className="h-calc($inset-y_+_$rounding) w-$inset-x bg-$trim-color"></div>
 			</div>
-			{/* Use negative margin-top here because of foreground obscure effect */}
-			{/* Use relative here because of background obscure effect */}
-			<div className="-mt-32 relative flex rounded-b-$rounding bg-$base-color shadow-$realistic-shadow [&_>_:nth-child(1)]:grow-1 [&_>_:nth-child(n_+_1)]:shrink-0">
+			{/* Use relative here because of the ribbon */}
+			<div className="mt-calc(-1_*_$inset-y) relative flex rounded-b-$rounding bg-$base-color shadow-$realistic-shadow [&_>_:nth-child(1)]:grow-1 [&_>_:nth-child(n_+_1)]:shrink-0">
 				{/* LHS */}
 				<div>
 					{/* Navigation bar */}
-					<div className="sticky t-32 h-72 bg-$base-color shadow-0_$hairline-thickness_0_0_$hairline-color">
+					<div className="sticky t-$inset-y h-$navigation-height bg-$base-color shadow-0_$hairline-thickness_0_0_$hairline-color">
 						<div>Hello, world!</div>
 					</div>
 					{iota(400).map(index =>
@@ -46,9 +44,9 @@ export function Home() {
 				</div>
 				{/* RHS */}
 				{/* <div className="w-400 rounded-r-$rounding bg-$gray-color shadow-inset_$hairline-thickness_0_0_$hairline-color ~[&_>_:nth-child(1)]:(bg-$gray-color shadow-inset_$hairline-thickness_0_0_$hairline-color)"> */}
-				<div className="w-400 rounded-br-$rounding bg-$gray-color shadow-inset_$hairline-thickness_0_0_$hairline-color">
+				<div className="w-$sidebar-width rounded-br-$rounding bg-$gray-color shadow-inset_$hairline-thickness_0_0_$hairline-color">
 					{/* Sidebar */}
-					<div className="sticky t-32">
+					<div className="sticky t-$inset-y">
 						{iota(10).map(index =>
 							<div key={index}>
 								Hello, world! ({index})
