4c91b71e8e0fbb01c7d17694b08f301f5c9e88eb
Zaydek <zaydekdotcom@gmail.com>
2023-01-10 07:26:34 -0800

    Progress

--- src/layout.tsx
+++ src/layout.tsx
@@ -4,23 +4,24 @@ import { PathContext } from "./router"
 export function Layout({ children }: PropsWithChildren) {
 	const path = useContext(PathContext)!
 
-	const [count, setCount] = useState(0)
+	//// const [count, setCount] = useState(0)
+	const [started, setStarted] = useState(false)
 
 	// On path changes...
 	useEffect(() => {
-		setCount(curr => curr + 1)
-		console.log("a")
-		//// return () => {
-		//// 	setCount(curr => curr + 1)
-		//// 	console.log("b")
-		//// }
+		setTimeout(() => {
+			setStarted(true)
+		}, 100)
+		return () => {
+			setStarted(false)
+		}
 	}, [path])
 
 	return <>
 		<div>
 			This is a wrapped component
 			<div style={{
-				transform: count % 2 === 1
+				transform: started
 					? "translateX(0px)"
 					: "translateX(-16px)",
 				transition: "transform 300ms ease",
