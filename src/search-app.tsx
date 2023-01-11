c1e6dd876cb81649a1776103c221d45c0e21b045
Zaydek <zaydekdotcom@gmail.com>
2023-01-11 07:09:13 -0800

    Remove dead code

--- src/search-app.tsx
+++ src/search-app.tsx
@@ -6,51 +6,7 @@ import { useEffect, useMemo, useRef, useState } from "react"
 import { manifest } from "./data/manifest-v2"
 import { Icon } from "./lib/react/icon"
 
-// name search v. tag search?
-// mode: filter / highlight?
-
-//// const names = Object.keys(manifest)
-
 type IconValue = keyof typeof manifest
-
-//// const isLower = (ch: string) => ch >= "a" && ch <= "z"
-
-//// function searchParts(str: string, substr: string) {
-//// 	if (substr.split("-"))
-////
-//// 	//// const index = str.indexOf(substr)
-//// 	//// if (index === -1 || (index > 0 && !isLower(str[index - 1]))) { return null }
-//// 	//// return [index, index + substr.length] as const
-//// }
-
-//// const isLower = (ch: string) => ch >= "a" && ch <= "z"
-//// const isUpper = (ch: string) => ch >= "A" && ch <= "Z"
-//// const isDigit = (ch: string) => ch >= "0" && ch <= "9"
-//// const isAlpha = (ch: string) => isLower(ch) || isUpper(ch) || isDigit(ch)
-////
-//// function splitParts(str: string) {
-//// 	const parts = []
-//// 	outer:
-//// 	for (let curr = 0; curr < str.length; curr++) {
-//// 		for (let next = curr; next < str.length; next++) {
-//// 			if ((isLower(str[next]) || isDigit(str[next])) &&        // E.g. [a-z0-9]
-//// 					(next + 1 < str.length && isUpper(str[next + 1]))) { // E.g. [A-Z]
-//// 				parts.push(str.slice(curr, next + 1))
-//// 				curr = next
-//// 				break
-//// 			} else if (!isAlpha(str[next]) &&                        // E.g. [^a-zA-Z0-9]
-//// 					(next + 1 < str.length && isAlpha(str[next + 1]))) { // E.g. [a-zA-Z0-9]
-//// 				parts.push(str.slice(curr, next + 0)) // Remove non-alpha
-//// 				curr = next
-//// 				break
-//// 			} else if (next + 1 === str.length) {
-//// 				parts.push(str.slice(curr))
-//// 				break outer // Done
-//// 			}
-//// 		}
-//// 	}
-//// 	return parts
-//// }
 
 function splitParts(str: string) {
 	return str.split(/(?=[A-Z])|[^a-zA-Z0-9]+/)
@@ -62,10 +18,6 @@ export function SearchApp() {
 
 	const inputRef = useRef<HTMLInputElement | null>(null)
 	const [search, setSearch] = useState("")
-
-	//// const zeroValues = useMemo(() => {
-	//// 	return Object.keys(manifest).map(name => [name, false] as readonly [name: IconValue, matches: boolean])
-	//// }, [])
 
 	const values = useMemo(() => {
 		const values: (readonly [name: IconValue, matches: boolean])[] = []
@@ -136,69 +88,31 @@ export function SearchApp() {
 					{debouncedValues
 						//// .filter(([, matches]) => matches)
 						.map(([name, matches], index) =>
-						//// <MemoGridItem
-						//// 	key={name}
-						//// 	name={name}
-						//// 	matches={matches}
-						//// 	hoverName={hoverName}
-						//// 	setHoverName={setHoverName}
-						//// />
-
-						<button
-							key={name}
-							className="flex justify-center align-center effect-icon-hover"
-							onPointerOver={e => {
-								setHoverName(name)
-							}}
-							onPointerLeave={e => {
-								setHoverName("")
-							}}
-						>
-							<Icon
-								className="h-40 w-40"
-								style={{
-									color: (matches || hoverName === name) ? "hsl(0, 0%, 10%)" : "hsl(0, 0%, 87.5%)",
-									transform: (matches || hoverName === name) ? "scale(1)" : "scale(0.875)",
-									//// transition: `100ms cubic-bezier(0, 1, 1, 1.25) ${index * 2}ms`,
-									//// transition: `20ms cubic-bezier(0, 1, 1, 1)`,
-
-									//// transition: `100ms cubic-bezier(0, 1, 1, 1.25)`,
-									transition: `50ms cubic-bezier(0, 1, 1, 2)`,
-									transitionProperty: "transform",
+							<button
+								key={name}
+								className="flex justify-center align-center effect-icon-hover"
+								onPointerOver={e => {
+									setHoverName(name)
 								}}
-								// @ts-expect-error
-								icon={feather[name]}
-							/>
-						</button>
-					)}
+								onPointerLeave={e => {
+									setHoverName("")
+								}}
+							>
+								<Icon
+									className="h-40 w-40"
+									style={{
+										color: (matches || hoverName === name) ? "hsl(0, 0%, 10%)" : "hsl(0, 0%, 87.5%)",
+										transform: (matches || hoverName === name) ? "scale(1)" : "scale(0.875)",
+										transition: `50ms cubic-bezier(0, 1, 1, 2)`,
+										transitionProperty: "transform",
+									}}
+									// @ts-expect-error
+									icon={feather[name]}
+								/>
+							</button>
+						)}
 				</div>
 			</div>
 		</div>
 	</>
 }
-
-//// const MemoGridItem = memo(function GridItem({ name, matches, hoverName, setHoverName }: { name: IconValue, matches: boolean, hoverName: IconValue | "", setHoverName: Dispatch<SetStateAction<IconValue | "">> }) {
-//// 	return <>
-//// 		<button
-//// 			className="flex justify-center align-center effect-icon-hover"
-//// 			onPointerOver={e => {
-//// 				setHoverName(name)
-//// 			}}
-//// 			onPointerLeave={e => {
-//// 				setHoverName("")
-//// 			}}
-//// 		>
-//// 			<Icon
-//// 				className="h-40 w-40"
-//// 				style={{
-//// 					color: (matches || hoverName === name) ? "hsl(0, 0%, 10%)" : "hsl(0, 0%, 90%)",
-//// 					transform: (matches || hoverName === name) ? "revert" : "scale(0.9)",
-//// 					transition: "100ms cubic-bezier(0, 1, 1, 1.25)",
-//// 					transitionProperty: "transform",
-//// 				}}
-//// 				// @ts-expect-error
-//// 				icon={feather[name]}
-//// 			/>
-//// 		</button>
-//// 	</>
-//// })
