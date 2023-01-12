5d9117f00460eb337cef9c124b511b5ccd4f7b2e
Zaydek <zaydekdotcom@gmail.com>
2023-01-12 05:15:35 -0800

    More polish

--- src/pages/home-backup.tsx
+++ src/pages/home-backup.tsx
@@ -57,7 +57,7 @@ function SearchBarContents() {
 
 function MainContents() {
 	return <>
-		<div className="py-$main-inset-y px-$main-inset-x">
+		<div className="pb-$main-inset-y px-$main-inset-x">
 			<div className="grid grid-cols-repeat(auto-fill,_minmax(96px,_1fr)) grid-auto-rows-96">
 				{iota(400).map(index =>
 					<div key={index} className="flex justify-center align-center">
@@ -111,16 +111,16 @@ export function Home() {
 						<div className="absolute bl-0 h-$rounding w-$rounding rounded-tr-1e3 bg-$base-color"></div>
 					</div>
 				</div>
-				<div className="mt-calc(-1_*_$rounding) flex rounded-b-$rounding bg-$base-color shadow-$realistic-shadow-6 [&_>_:nth-child(1)]:grow-1">
+				<div className="mt-calc(-1_*_$rounding) flex rounded-b-$rounding bg-$base-color shadow-$shadow-3 [&_>_:nth-child(1)]:grow-1">
 					<main>
-						<div className="px-$main-inset-x py-$main-inset-y sticky t-$ribbon-inset-y [background-image]-linear-gradient($base-color_calc($main-inset-y_+_$search-bar-height),_transparent)">
+						<div className="py-$main-inset-y px-$main-inset-x sticky t-$ribbon-inset-y [background-image]-linear-gradient($base-color_calc($main-inset-y_+_$search-bar-height),_transparent)">
 							<div className="h-$search-bar-height rounded-1e3 bg-hsl($base-h,_$base-s,_95%) [&:hover]:(bg-$base-color shadow-$shadow-2)">
 								{/* <div>Hello</div> */}
 							</div>
 						</div>
 						<MainContents />
 					</main>
-					<aside className="w-$sidebar-2-width shadow-$inset-hairline-shadow-l">
+					<aside className="w-$sidebar-width shadow-$inset-hairline-shadow-l">
 						<div className="sticky t-$ribbon-inset-y z-10 flex flex-col">
 							<Sidebar2Contents />
 						</div>
