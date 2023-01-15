1e29530f3a4c907064732017829158eab1c275eb
Zaydek <zaydekdotcom@gmail.com>
2023-01-15 10:34:11 -0800

    Transform based size 💪

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export const sizeInitial        = 32
 export const sizeMin            = sizeInitial - 16
 export const sizeMax            = sizeInitial + 16
-export const sizeStep           = 1
+export const sizeStep           = 0.125
 
 export const strokeWidthInitial = 2
 export const strokeWidthMin     = strokeWidthInitial - 1.5
 export const strokeWidthMax     = strokeWidthInitial + 1.5
-export const strokeWidthStep    = 0.125
+export const strokeWidthStep    = 0.025
--- src/search-app.tsx
+++ src/search-app.tsx
@@ -182,7 +182,7 @@ function SearchResultsContents({ setSelected }: { setSelected: Dispatch<SetState
 				<div className="flex flex-col" onClick={e => setSelected(name as keyof typeof manifest)}>
 					<div className="flex flex-center h-80">
 						{/* TODO: Use x as keyof typeof feather because of Object.keys */}
-						<Icon className="h-$size w-$size [stroke-width]-$stroke-width [color]-#333" icon={feather[name as keyof typeof feather]} />
+						<Icon className="h-32 w-32 [transform]-scale($scale) [stroke-width]-$stroke-width [color]-#333" icon={feather[name as keyof typeof feather]} />
 					</div>
 					<div className="flex flex-center wrap-wrap h-16 [text-align]-center [font-size]-12 [-webkit-user-select]-all [user-select]-all">
 						{/* TODO: Use x as keyof typeof feather because of Object.keys */}
@@ -287,7 +287,7 @@ function App() {
 	const [strokeWidth, setStrokeWidth] = useState(strokeWidthInitial)
 
 	useEffect(() => {
-		document.body.style.setProperty("--size", `${size}px`)
+		document.body.style.setProperty("--scale", `${size / sizeInitial}`)
 	}, [size])
 
 	useEffect(() => {
@@ -343,9 +343,8 @@ function App() {
 							</div>
 						</Tooltip>
 					</div>
-					{/* Here */}
 					<div className="flex flex-center aspect-2">
-						<Icon className="h-calc($size_*_2) w-calc($size_*_2) [stroke-width]-$stroke-width [color]-#333" icon={feather[selected]} />
+						<Icon className="h-64 w-64 [transform]-scale($scale) [stroke-width]-$stroke-width [color]-#333" icon={feather[selected]} />
 					</div>
 					<Checkbox checked={showIcon} setChecked={setShowIcon}>
 						<div>
