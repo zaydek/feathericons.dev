454f63f9246adafc72d3525fe99ffbba0ebb9a21
Zaydek <zaydekdotcom@gmail.com>
2023-01-15 10:02:07 -0800

    Add aria lib and preview icon sans sliders working

--- src/search-app.tsx
+++ src/search-app.tsx
@@ -9,6 +9,14 @@ import { manifest } from "./data/react-feather-manifest@4.29.0"
 import { detab } from "./lib/format"
 import { Icon } from "./lib/react/icon"
 
+function CAPS({ children }: PropsWithChildren) {
+	return <>
+		<div className="[white-space]-pre [font]-700_10px_/_normal_$sans [letter-spacing]-0.1em [color]-#333">
+			{children}
+		</div>
+	</>
+}
+
 function useRestorableState<T>(initialValue: T, zeroValue: T) {
 	const [state, setState] = useState(initialValue)
 	const [history, setHistory] = useState([state])
@@ -74,7 +82,7 @@ function useRestorableState<T>(initialValue: T, zeroValue: T) {
 }
 
 // TODO: Make icon required?
-function Tooltip({ icon, content, data, children, ...props }: { icon?: typeof Icon, content: ReactNode, data?: any } & HTMLAttributes<HTMLElement>) {
+function Tooltip({ pos, icon, tip, data, children, ...props }: { pos: "start" | "end", icon?: typeof Icon, tip: ReactNode, data?: any } & HTMLAttributes<HTMLElement>) {
 	const [show, setShow] = useState(true)
 
 	// This is a trick to hide the tooltip on data changes
@@ -90,7 +98,10 @@ function Tooltip({ icon, content, data, children, ...props }: { icon?: typeof Ic
 		<div className="relative" data-group {...props}>
 			{children}
 			{show && <>
-				<div className="py-12 absolute t-100% l-50% [transform]-translateX(-50%) [pointer-events]-none">
+				<div className={pos === "start"
+					? "py-12 absolute t-100% l-0 [pointer-events]-none"
+					: "py-12 absolute t-100% r-0 [pointer-events]-none"
+				}>
 					<div className={detab(`
 						[transform]-translateY(8px)
 						[opacity]-0
@@ -102,9 +113,9 @@ function Tooltip({ icon, content, data, children, ...props }: { icon?: typeof Ic
 							<div className="px-12 flex align-center gap-10 h-32 rounded-12 [background-color]-hsl(0,_0%,_99%) [box-shadow]-$shadow-6,_$realistic-shadow-6">
 								{/* TODO: Draw icon here */}
 								<div className="h-16 w-16 rounded-1e3 [background-color]-#666"></div>
-								<div className="[white-space]-pre [font]-700_10px_/_normal_$sans [letter-spacing]-0.1em [color]-#333">
-									{content}
-								</div>
+								<CAPS>
+									{tip}
+								</CAPS>
 							</div>
 							{/* <div className="absolute -t-2 x-0 flex justify-center">
 								<div className="h-8 w-8 rounded-2 [background-color]-#fff [transform]-rotate(45deg)"></div>
@@ -117,10 +128,18 @@ function Tooltip({ icon, content, data, children, ...props }: { icon?: typeof Ic
 	</>
 }
 
-function SearchBarButton(props: HTMLAttributes<HTMLElement>) {
+// TODO: Use button props here?
+//
+// NOTE: We can't use :first-of-type and :last-of-type because of <Tooltip>
+// wrappers
+function SearchBarButton({ start, end, ...props }: { start?: boolean, end?: boolean } & HTMLAttributes<HTMLElement>) {
+	[start, end] = [start ?? false, end ?? false]
+
 	return <>
-		<div className="flex flex-center h-32 w-32 rounded-1e3 [background-color]-pink" {...props}>
-			<div className="h-16 w-16 rounded-1e3 [background-color]-red"></div>
+		<div className="px-8 flex align-center h-$search-bar-height" style={{ paddingLeft: start ? 16 : undefined, paddingRight: end ? 16 : undefined }}>
+			<div className="flex flex-center h-32 w-32 rounded-1e3 [background-color]-pink" {...props}>
+				<div className="h-16 w-16 rounded-1e3 [background-color]-red"></div>
+			</div>
 		</div>
 	</>
 }
@@ -149,13 +168,15 @@ function Highlight({ indexes, children }: { indexes: readonly [number, number] |
 	}
 }
 
-function SearchResultsContents() {
+// TODO
+function SearchResultsContents({ setSelected }: { setSelected: Dispatch<SetStateAction<keyof typeof manifest>> }) {
 	const { searchResults } = useContext(SearchContext)!
 
 	return <>
 		<div className="grid grid-cols-repeat(auto-fill,_minmax(96px,_1fr))">
 			{Object.keys(searchResults).map(name => <Fragment key={name}>
-				<div id={name} className="flex flex-col">
+				{/* <div id={name} className="flex flex-col" onMouseEnter={e => setSelected(name as keyof typeof manifest)}> */}
+				<div id={name} className="flex flex-col" onClick={e => setSelected(name as keyof typeof manifest)}>
 					<div className="flex flex-center h-80">
 						{/* TODO: Use x as keyof typeof feather because of Object.keys */}
 						<Icon className="h-32 w-32 [color]-#333" icon={feather[name as keyof typeof feather]} />
@@ -172,13 +193,79 @@ function SearchResultsContents() {
 	</>
 }
 
+//// function Section({ children, ...props }: HTMLAttributes<HTMLElement>) {
+//// 	return <>
+//// 		{/* <section className="py-$sidebar-inset-y [&:nth-child(1)]:pt-$main-inset-y px-$sidebar-inset-x flex flex-col gap-$sidebar-label-height" {...props}> */}
+//// 		<section className="py-$sidebar-inset-y px-$sidebar-inset-x flex flex-col gap-$sidebar-label-height" {...props}>
+//// 			{children}
+//// 		</section>
+//// 	</>
+//// }
+
+//// function Label({ tip, children }: PropsWithChildren<{ tip: ReactNode }>) {
+function Label({ children }: PropsWithChildren) {
+	return <>
+		<div className="flex justify-space-between align-center h-$sidebar-label-height">
+			<div className="flex align-center gap-10">
+				{children}
+			</div>
+			{/* <div className="px-4">
+				<div className="flex flex-center h-24 w-24 rounded-1e3 [background-color]-$light-placeholder-color">
+					<div className="h-50% aspect-1 rounded-1e3 [background-color]-$dark-placeholder-color"></div>
+				</div>
+			</div> */}
+			{/* TODO: Extract? */}
+			{/* <Tooltip pos="end" tip={tip}> */}
+			<div className="flex flex-center h-32 w-32 rounded-1e3 [background-color]-#eee [&:hover]:([background-color]-#fff [box-shadow]-$shadow-2)">
+				<div className="h-16 w-16 rounded-1e3 [background-color]-#aaa"></div>
+			</div>
+			{/* </Tooltip> */}
+		</div>
+	</>
+}
+
+function Hairline() {
+	return <hr className="h-$hairline-height [background-color]-$hairline-color" />
+}
+
+function Checkbox({ checked, setChecked, children }: PropsWithChildren<{ checked: boolean, setChecked: Dispatch<SetStateAction<boolean>> }>) {
+	return <>
+		<div className="flex justify-space-between align-center h-$sidebar-label-height" onClick={e => setChecked(curr => !curr)}>
+			{children}
+			<div className="flex flex-col justify-center h-$sidebar-label-height">
+				<div className={`flex ${checked ? "justify-end" : "justify-start"} align-center h-12 w-48 rounded-1e3 ${checked ? "[background-color]-$alt-trim-color" : "[background-color]-$hairline-color"}`}>
+					<div className="flex flex-center h-$sidebar-input-height w-$sidebar-input-height rounded-1e3 [background-color]-$base-color [box-shadow]-$shadow-6">
+						<div className="h-50% aspect-1 rounded-1e3 [background-color]-$placeholder-color"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</>
+}
+
+function Slider() {
+	return <>
+		<div className="px-4">
+			<div className="flex flex-col justify-center h-$sidebar-label-height">
+				<div className="flex flex-center h-6 rounded-1e3 [background-color]-$alt-trim-color">
+					<div className="flex flex-center h-calc($sidebar-input-height_+_4px) w-calc($sidebar-input-height_+_4px) rounded-1e3 [background-color]-$base-color [box-shadow]-$shadow-6">
+						<div className="h-50% aspect-1 rounded-1e3 [background-color]-$placeholder-color"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</>
+}
+
 // TODO
 document.documentElement.style.backgroundColor = "#fff"
 
-function SearchApp() {
-	//// const { search, setSearch, restoreSearch } = useContext(SearchContext)!
-	const { search, setSearch } = useContext(SearchContext)!
+function App() {
+	const { search, setSearch, restoreSearch } = useContext(SearchContext)!
 	const { sidebarOrder, setSidebarOrder } = useContext(SidebarContext)!
+
+	const [selected, setSelected] = useState<keyof typeof manifest>("Feather")
+	const [showIcon, setShowIcon] = useState(false)
 
 	return <>
 		{/* <a href={featherZip} download>
@@ -187,47 +274,70 @@ function SearchApp() {
 		<div className="p-32 flex justify-center">
 			<div className="basis-2e3 flex gap-64 [&_>_:nth-child(1)]:grow-1">
 				<div className="flex flex-col gap-64">
-					<div className="px-16 flex align-center h-64 rounded-1e3 [background-color]-#eee [&:is(:hover,_:focus-within)]:([background-color]-#fff [box-shadow]-$shadow-2) [&_>_:nth-child(2)]:grow-1">
-						<Tooltip content={<>SEARCH ICONS&nbsp;&nbsp;<span className="[opacity]-0.75">CTRL+/</span></>}>
-							<SearchBarButton />
+					<div className="flex align-center h-64 rounded-1e3 [background-color]-#eee [&:is(:hover,_:focus-within)]:([background-color]-#fff [box-shadow]-$shadow-2) [&_>_:nth-child(2)]:grow-1">
+						<Tooltip pos="start" tip={<>SEARCH ICONS&nbsp;&nbsp;<span className="[opacity]-0.75">CTRL+/</span></>}>
+							<SearchBarButton start />
 						</Tooltip>
 						<input
-							className="px-16 h-64"
+							className="px-8 h-64"
 							type="text"
 							value={search}
 							onChange={e => setSearch(e.currentTarget.value)}
 							onKeyDown={e => {
 								if (e.key === "ArrowUp") {
 									e.preventDefault()
-									//// restoreSearch(-1)
+									restoreSearch(-1)
 								} else if (e.key === "ArrowDown") {
 									e.preventDefault()
-									//// restoreSearch(+1)
+									restoreSearch(+1)
 								}
 							}}
 							autoFocus
 						/>
-						<Tooltip content={<>TOGGLE DARK MODE&nbsp;&nbsp;<span className="[opacity]-0.75">CTRL+D</span></>}>
-							<SearchBarButton />
+						<Tooltip pos="end" tip={`USE ${sidebarOrder === "forwards" ? "RTL" : "LTR"} LAYOUT`} data={sidebarOrder} onClick={e => setSidebarOrder(curr => curr === "forwards" ? "backwards" : "forwards")}>
+							<SearchBarButton end />
 						</Tooltip>
+						{/* <Tooltip content={<>TOGGLE DARK MODE&nbsp;&nbsp;<span className="[opacity]-0.75">CTRL+D</span></>}>
+							<SearchBarButton end />
+						</Tooltip> */}
 					</div>
-					<SearchResultsContents />
+					<SearchResultsContents setSelected={setSelected} />
 				</div>
-				<div className="flex flex-col gap-20 w-300" style={{ order: sidebarOrder === "forwards" ? undefined : -1 }}>
-					<div className="flex justify-end align-center h-64 [&_>_:nth-child(1)]:grow-1">
-						<div className="flex align-center gap-10">
-							<div className="h-16 w-16 rounded-1e3 [background-color]-#aaa"></div>
-							<div>Hello, world!</div>
+				<div className="flex flex-col gap-20 w-350" style={{ order: sidebarOrder === "forwards" ? undefined : -1 }}>
+					<div className="flex align-center h-64 [&_>_:nth-child(1)]:grow-1">
+						<div className="flex align-center gap-10 h-20">
+							<Icon className="h-16 w-16 [color]-#333" icon={feather[selected]} />
+							<div>{selected}</div>
 						</div>
-						<Tooltip content={<>MOVE SIDEBAR&nbsp;&nbsp;<span className="[opacity]-0.75">CTRL+\</span></>} data={sidebarOrder} onClick={e => setSidebarOrder(curr => curr === "forwards" ? "backwards" : "forwards")}>
-							{/* TODO: Remove [cursor]-pointer */}
-							<div className="flex flex-center h-32 w-32 rounded-1e3 [background-color]-#eee [cursor]-pointer [&:hover]:([background-color]-#fff [box-shadow]-$shadow-2)">
+						<Tooltip pos="end" tip="OPEN ICON DOCS">
+							<div className="flex flex-center h-32 w-32 rounded-1e3 [background-color]-#eee [&:hover]:([background-color]-#fff [box-shadow]-$shadow-2)">
 								<div className="h-16 w-16 rounded-1e3 [background-color]-#aaa"></div>
 							</div>
 						</Tooltip>
 					</div>
 					{/* Here */}
-					<div className="h-64 w-64 rounded-1e3 [background-color]-#333"></div>
+					<div className="flex flex-center aspect-2">
+						<Icon className="h-64 w-64 [color]-#333" icon={feather[selected]} />
+					</div>
+					<Checkbox checked={showIcon} setChecked={setShowIcon}>
+						<div>
+							Code
+						</div>
+					</Checkbox>
+					<Hairline />
+					<Label>
+						<div>
+							Size
+						</div>
+					</Label>
+					<Slider />
+					<Hairline />
+					<Label>
+						<div>
+							Stroke Width
+						</div>
+					</Label>
+					<Slider />
 				</div>
 			</div>
 		</div>
@@ -237,7 +347,7 @@ function SearchApp() {
 const SearchContext = createContext<{
 	search:          string
 	setSearch:       Dispatch<SetStateAction<string>>
-	//// restoreSearch:   (_: number) => void
+	restoreSearch:   (_: number) => void
 	searchResults:   Partial<Record<keyof typeof feather, readonly [number, number] | null>>
 } | null>(null)
 
@@ -261,8 +371,8 @@ function getSubstringIndexes(str: string, substr: string) {
 }
 
 function StateProvider({ children }: PropsWithChildren) {
-	//// const [search, setSearch, restoreSearch] = useRestorableState("", "")
-	const [search, setSearch] = useState("")
+	//// const [search, setSearch] = useState("")
+	const [search, setSearch, restoreSearch] = useRestorableState("", "")
 
 	const $$search = useMemo(() => {
 		return search
@@ -317,10 +427,9 @@ function StateProvider({ children }: PropsWithChildren) {
 		<SearchContext.Provider value={useMemo(() => ({
 			search,
 			setSearch,
-			//// restoreSearch,
+			restoreSearch,
 			searchResults,
-		//// }), [restoreSearch, search, searchResults, setSearch])}>
-		}), [search, searchResults, setSearch])}>
+		}), [restoreSearch, search, searchResults, setSearch])}>
 			<SidebarContext.Provider value={useMemo(() => ({
 				sidebarOrder,
 				setSidebarOrder,
@@ -331,10 +440,10 @@ function StateProvider({ children }: PropsWithChildren) {
 	</>
 }
 
-export function ProvidedSearchApp() {
+export function ProvidedApp() {
 	return <>
 		<StateProvider>
-			<SearchApp />
+			<App />
 		</StateProvider>
 	</>
 }
