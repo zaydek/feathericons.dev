0d9960978426326120027347f504803a086bad47
Zaydek <zaydekdotcom@gmail.com>
2023-01-19 13:41:41 -0800

    Oops

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
-import "the-new-css-reset";
+import "the-new-css-reset"
 
-import "./css/base.scss";
-import "./css/vars.scss";
+import "./css/base.scss"
+import "./css/vars.scss"
 
-import "uno.css";
+import "uno.css"
 
-import { createRoot } from "react-dom/client";
-import { App } from "./app";
+import { createRoot } from "react-dom/client"
+import { App } from "./app"
 //// import { ProvidedApp } from "./search-app"
 
-const root = document.getElementById("root")!;
+const root = document.getElementById("root")!
 if (import.meta.env.DEV) {
-	console.log("[DEBUG] createRoot");
-	createRoot(root).render(<App initialPath={window.location.pathname} />);
+	console.log("[DEBUG] createRoot")
+	createRoot(root).render(<App initialPath={window.location.pathname} />)
 	//// createRoot(root).render(<ProvidedApp />)
 } else {
-	console.log("[DEBUG] hydrateRoot");
-	createRoot(root, <App initialPath={window.location.pathname} />);
+	console.log("[DEBUG] hydrateRoot")
+	createRoot(root).render(<App initialPath={window.location.pathname} />)
 	//// createRoot(root).render(<ProvidedApp />)
 }
