fe535842785e4a14dd2aeb97fd711a14e3061d6e
Zaydek <zaydekdotcom@gmail.com>
2023-01-19 13:40:27 -0800

    Restore full layout for backup to Netlify

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
-import "the-new-css-reset"
+import "the-new-css-reset";
 
-import "./css/base.scss"
-import "./css/vars.scss"
+import "./css/base.scss";
+import "./css/vars.scss";
 
-import "uno.css"
+import "uno.css";
 
-import { createRoot } from "react-dom/client"
-//// import { App } from "./app"
-import { ProvidedApp } from "./search-app"
+import { createRoot } from "react-dom/client";
+import { App } from "./app";
+//// import { ProvidedApp } from "./search-app"
 
-const root = document.getElementById("root")!
+const root = document.getElementById("root")!;
 if (import.meta.env.DEV) {
-	console.log("[DEBUG] createRoot")
-	//// createRoot(root).render(<App initialPath={window.location.pathname} />)
-	createRoot(root).render(<ProvidedApp />)
+	console.log("[DEBUG] createRoot");
+	createRoot(root).render(<App initialPath={window.location.pathname} />);
+	//// createRoot(root).render(<ProvidedApp />)
 } else {
-	console.log("[DEBUG] hydrateRoot")
-	//// hydrateRoot(root, <App initialPath={window.location.pathname} />)
-	createRoot(root).render(<ProvidedApp />)
+	console.log("[DEBUG] hydrateRoot");
+	createRoot(root, <App initialPath={window.location.pathname} />);
+	//// createRoot(root).render(<ProvidedApp />)
 }
