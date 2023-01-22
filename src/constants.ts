43402cefb6405e8a0fd14f751472b6b7f884ddde
Zaydek <zaydekdotcom@gmail.com>
2023-01-21 20:55:08 -0800

    Fix bug

--- src/constants.ts
+++ src/constants.ts
@@ -24,7 +24,7 @@ export const jsxPlaceholder = detab(`
 
 export const tsxPlaceholder = detab(`
 	// https://feathericons.dev/feather
-	export function Feather(props: SVGAttributes<SVGElement>) {
+	export function Feather(props: JSX.IntrinsicElements["svg"]) {
 		return (
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" {...props}>
 				<path d="M20.24 12.24a6 6 0 0 0-8.49-8.49L5 10.5V19h8.5z" />
