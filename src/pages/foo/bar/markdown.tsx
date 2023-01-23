733f3d1f6f36327533b9ad22a97537d801df5d29
Zaydek <zaydekdotcom@gmail.com>
2023-01-23 05:07:47 -0800

    Runtime syntax highlighting working

--- src/pages/foo/bar/markdown.tsx
+++ src/pages/foo/bar/markdown.tsx
@@ -1,7 +1,8 @@
 import * as feather from "../../../data/react-feather"
 
 import { MDXProvider } from "@mdx-js/react"
-import { ReactNode } from "react"
+import { Fragment, ReactElement, ReactNode, useEffect, useState } from "react"
+import { getHighlighter, Highlighter, IThemedToken } from "shiki-es"
 import { ThickIcon } from "../../../typography"
 import Markdown from "./_markdown.mdx"
 
@@ -36,13 +37,16 @@ function H1({ children, ...props }: JSX.IntrinsicElements["h1"]) {
 	return (
 		<h1
 			id={id}
-			className="relative text-gray-900 [&:not(:first-child)]:mt-32 [&:not(:first-child)]:scroll-mt-32"
+			className="relative text-gray-900
+				[&:not(:first-child)]:mt-32
+				[&:not(:first-child)]:scroll-mt-32"
 			{...props}
 		>
 			{children}
 			<a
 				href={href}
-				className="absolute top-0 right-[100%] bottom-0 flex items-center px-10 opacity-0 [h1:hover_&]:opacity-100"
+				className="absolute top-0 right-[100%] bottom-0 flex items-center px-10
+					opacity-0 [h1:hover_&]:opacity-100"
 			>
 				<ThickIcon className="h-20 w-20 text-[var(--trim-color)]" icon={feather.Link2} />
 			</a>
@@ -56,13 +60,16 @@ function H2({ children, ...props }: JSX.IntrinsicElements["h1"]) {
 	return (
 		<h2
 			id={id}
-			className="relative text-gray-800 [&:not(:first-child)]:mt-32 [&:not(:first-child)]:scroll-mt-32"
+			className="relative text-gray-800
+				[&:not(:first-child)]:mt-32
+				[&:not(:first-child)]:scroll-mt-32"
 			{...props}
 		>
 			{children}
 			<a
 				href={href}
-				className="absolute top-0 right-[100%] bottom-0 flex items-center px-10 opacity-0 [h2:hover_&]:opacity-100"
+				className="absolute top-0 right-[100%] bottom-0 flex items-center px-10
+					opacity-0 [h2:hover_&]:opacity-100"
 			>
 				<ThickIcon className="h-20 w-20 text-[var(--trim-color)]" icon={feather.Link2} />
 			</a>
@@ -70,15 +77,117 @@ function H2({ children, ...props }: JSX.IntrinsicElements["h1"]) {
 	)
 }
 
+//// function IconPreview() {
+//// 	const { selectedName, viewSource, formatAs, clipboard } = useContext(SelectedContext)!
+////
+//// 	const [highlighter, setHighlighter] = useState<Highlighter | null>(null)
+//// 	const [tokens, setTokens] = useState<IThemedToken[][] | null>(null)
+////
+//// 	useEffect(() => {
+//// 		async function init() {
+//// 			const highlighter = await getHighlighter({ theme: "github-light" })
+//// 			setHighlighter(highlighter)
+//// 		}
+//// 		init()
+//// 	}, [])
+////
+//// 	useEffect(() => {
+//// 		if (highlighter === null) { return } // prettier-ignore
+//// 		const tokens = highlighter.codeToThemedTokens(clipboard + "\n", formatAs === "svg" ? "xml" : "tsx", undefined, {
+//// 			includeExplanation: false,
+//// 		})
+//// 		setTokens(tokens)
+//// 	}, [clipboard, formatAs, highlighter])
+////
+//// 	return viewSource ? (
+//// 		// Use overflow-x-scroll > inline-block p-* because of overflow-x bug
+//// 		<pre className="min-h-256 overflow-x-scroll rounded-24 bg-white text-gray-800 [box-shadow:_var(--shadow-2)]">
+//// 			<code className="inline-block p-24">
+//// 				{tokens === null ? (
+//// 					<div className="text-gray-400">Initializing shiki-es…</div>
+//// 				) : (
+//// 					tokens.map((token, y) => (
+//// 						<Fragment key={y}>
+//// 							<div className="relative -mx-[1ch] pl-[4ch]">
+//// 								<div className="absolute top-0 bottom-0 left-0 select-none">
+//// 									<div className="w-[2ch] text-right text-gray-300">{y + 1}</div>
+//// 								</div>
+//// 								{token.map(({ content, color }, x) => (
+//// 									<span key={x} style={{ color }}>
+//// 										{y === 0 ? <Anchor>{content}</Anchor> : content}
+//// 									</span>
+//// 								))}
+//// 							</div>
+//// 						</Fragment>
+//// 					))
+//// 				)}
+//// 			</code>
+//// 		</pre>
+//// 	) : (
+//// 		<div className="dots-pattern flex aspect-[1.5] items-center justify-center rounded-24 bg-white [box-shadow:_var(--shadow-2)]">
+//// 			<Icon
+//// 				className="h-64 w-64 text-gray-800 [stroke-width:_var(--stroke-width)] [transform:_scale(var(--scale))]"
+//// 				icon={feather[selectedName]}
+//// 			/>
+//// 		</div>
+//// 	)
+//// }
+
 function Pre({ children, ...props }: JSX.IntrinsicElements["pre"]) {
+	const $children = children as ReactElement<{ className: string; children: string }>
+
+	const [lang, code] = [$children.props.className.slice("language-".length), $children.props.children] as const
+
+	const [highlighter, setHighlighter] = useState<Highlighter | null>(null)
+	const [tokens, setTokens] = useState<IThemedToken[][] | null>(null)
+
+	useEffect(() => {
+		async function init() {
+			const highlighter = await getHighlighter({ theme: "github-light" })
+			setHighlighter(highlighter)
+		}
+		init()
+	}, [])
+
+	useEffect(() => {
+		if (highlighter === null) { return } // prettier-ignore
+		const tokens = highlighter.codeToThemedTokens(code, lang, undefined, {
+			includeExplanation: false,
+		})
+		setTokens(tokens)
+		console.log(tokens)
+	}, [code, highlighter, lang])
+
 	return (
 		// TODO: Add overflow here?
 		// Syntax highlighting?
 		// Line numbers?
-		<pre className="-mx-24 my-10 rounded-24 bg-gray-900 p-24 text-gray-200" {...props}>
-			<code>
-				{/* @ts-expect-error */}
-				{children.props.children}
+		// <pre className="-mx-24 my-10 rounded-24 bg-gray-900 p-24 text-gray-200" {...props}>
+		// 	<code>
+		// 		{/* @ts-expect-error */}
+		// 		{children.props.children}
+		// 	</code>
+		// </pre>
+		<pre className="min-h-256 overflow-x-scroll rounded-24 bg-white text-gray-800 [box-shadow:_var(--shadow-2)]">
+			<code className="inline-block p-24">
+				{tokens === null ? (
+					<div className="text-gray-400">Initializing shiki-es…</div>
+				) : (
+					tokens.map((token, y) => (
+						<Fragment key={y}>
+							<div className="relative -mx-[1ch] pl-[4ch]">
+								<div className="absolute top-0 bottom-0 left-0 select-none">
+									<div className="w-[2ch] text-right text-gray-300">{y + 1}</div>
+								</div>
+								{token.map(({ content, color }, x) => (
+									<span key={x} style={{ color }}>
+										{y === 0 ? <Anchor>{content}</Anchor> : content}
+									</span>
+								))}
+							</div>
+						</Fragment>
+					))
+				)}
 			</code>
 		</pre>
 	)
