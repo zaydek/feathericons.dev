85182853faf4128acb7ac38fb4e2fd0c0aea0fed
Zaydek <zaydekdotcom@gmail.com>
2023-01-25 03:16:14 -0800

    Next doesn't like <Head> in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+// TODO: Move to _document.tsx?
 import "../css/tailwind.css"
 
 import "../css/misc.scss"
@@ -5,12 +6,15 @@ import "../css/type.scss"
 import "../css/vars.scss"
 
 import { AppProps } from "next/app"
+import { Head } from "next/document" // eslint-disable-line @next/next/no-document-import-in-page
+import { toKebabCase } from "../lib/cases"
 import { SharedApp } from "../shared-app"
 import { ShikiProvider } from "../shiki"
 import { StateProvider } from "../state"
 
 // TODO: Move to _document.tsx?
 import { Fira_Code, Inter } from "@next/font/google"
+import { IconProps } from "./[icon]"
 
 // TODO
 const inter = Inter({
@@ -24,13 +28,44 @@ const firaCode = Fira_Code({
 	variable: "--font-fira-code",
 })
 
-export default function App({ pageProps, Component }: AppProps) {
+export default function App({ pageProps }: AppProps<undefined | IconProps>) {
+	const name = pageProps?.name
+
+	//// // Shorten <title>
+	//// useEffect(() => {
+	//// 	if (name !== undefined) { return } // prettier-ignore
+	//// 	document.title = "Feather"
+	//// }, [name])
+
 	return (
-		<ShikiProvider>
-			<StateProvider>
-				{/* TODO: Remove debug JSON */}
-				<SharedApp {...pageProps}>{JSON.stringify(pageProps, null, 2)}</SharedApp>
-			</StateProvider>
-		</ShikiProvider>
+		<>
+			<Head>
+				{/* SEO */}
+				{/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+				<title>{name === undefined ? "Feather – Simply beautiful open source icons" : `Icon: ${name}`}</title>
+				<meta name="description" content="Feather is a collection of simply beautiful open source icons. Each icon is designed on a 24x24 grid with an emphasis on simplicity, consistency and readability." />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href={`/feather/${toKebabCase(name ?? "Feather")}.svg`} />
+
+				{/* SEO / Facebook */}
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://feathericons.dev" />
+				<meta property="og:title" content="Feather – Simply beautiful open source icons" />
+				<meta property="og:description" content="Feather is a collection of simply beautiful open source icons. Each icon is designed on a 24x24 grid with an emphasis on simplicity, consistency and readability." />
+				<meta property="og:image" content="https://feathericons.dev/feather-og.png" />
+
+				{/* SEO / Twitter */}
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://feathericons.dev" />
+				<meta property="twitter:title" content="Feather – Simply beautiful open source icons" />
+				<meta property="twitter:description" content="Feather is a collection of simply beautiful open source icons. Each icon is designed on a 24x24 grid with an emphasis on simplicity, consistency and readability." />
+				<meta property="twitter:image" content="https://feathericons.dev/feather-og.png" />
+			</Head>
+			<ShikiProvider>
+				<StateProvider>
+					<SharedApp name={name} />
+				</StateProvider>
+			</ShikiProvider>
+		</>
 	)
 }
--- src/shared-app.tsx
+++ src/shared-app.tsx
@@ -1,6 +1,5 @@
-import Head from "next/head"
 import Link, { LinkProps } from "next/link"
-import { PropsWithChildren, useEffect } from "react"
+import { PropsWithChildren } from "react"
 import { manifest } from "./data/react-feather-manifest"
 import { toKebabCase } from "./lib/cases"
 import { IconProps } from "./pages/[icon]"
@@ -14,37 +13,8 @@ function OrangeLink({ children, ...props }: PropsWithChildren<LinkProps>) {
 }
 
 export function SharedApp({ name }: Partial<IconProps>) {
-	// Shorten <title>
-	useEffect(() => {
-		if (name !== undefined) { return } // prettier-ignore
-		document.title = "Feather"
-	}, [name])
-
 	return (
 		<>
-			<Head>
-				{/* SEO */}
-				<title>{name === undefined ? "Feather – Simply beautiful open source icons" : `Icon: ${name}`}</title>
-				<meta name="description" content="Feather is a collection of simply beautiful open source icons. Each icon is designed on a 24x24 grid with an emphasis on simplicity, consistency and readability." />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href={`/feather/${toKebabCase(name ?? "Feather")}.svg`} />
-
-				{/* SEO / Facebook */}
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://feathericons.dev" />
-				<meta property="og:title" content="Feather – Simply beautiful open source icons" />
-				<meta property="og:description" content="Feather is a collection of simply beautiful open source icons. Each icon is designed on a 24x24 grid with an emphasis on simplicity, consistency and readability." />
-				<meta property="og:image" content="https://feathericons.dev/feather-og.png" />
-
-				{/* SEO / Twitter */}
-				<meta property="twitter:card" content="summary_large_image" />
-				<meta property="twitter:url" content="https://feathericons.dev" />
-				<meta property="twitter:title" content="Feather – Simply beautiful open source icons" />
-				<meta property="twitter:description" content="Feather is a collection of simply beautiful open source icons. Each icon is designed on a 24x24 grid with an emphasis on simplicity, consistency and readability." />
-				<meta property="twitter:image" content="https://feathericons.dev/feather-og.png" />
-			</Head>
-
-			{/* Home */}
 			<div className="p-16">
 				<nav className="flex flex-wrap gap-4">
 					<OrangeLink href="/" aria-label="Navigate to home">
