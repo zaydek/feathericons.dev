fd501432fae52368393beac97d567631258a3197
Zaydek <zaydekdotcom@gmail.com>
2023-01-27 15:44:49 -0800

    Add underline on hover for navigation

--- src/layout-search-results.tsx
+++ src/layout-search-results.tsx
@@ -96,8 +96,11 @@ const MemoGridItem = memo(function GridItem({ name, indexes }: { name: keyof typ
 					setSelectedSvgElement(document.getElementById(name)! as Element as SVGSVGElement)
 				}}
 			>
-				<div className="flex h-16 items-center justify-center truncate px-4">
-					<TypographySmallSans className="truncate text-gray-800">
+				<div className="group/name flex h-16 items-center justify-center truncate px-4">
+					<TypographySmallSans
+						className="truncate text-gray-800
+							group-hover/name:underline group-hover/name:decoration-gray-400"
+					>
 						<Highlight indexes={indexes}>{name}</Highlight>
 					</TypographySmallSans>
 				</div>
