696b7fe268726d18148908df9f0f89c426268b7e
Zaydek <zaydekdotcom@gmail.com>
2023-01-28 05:57:04 -0800

    Finally get fonts working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,34 +3,45 @@ import "../css/_tailwind.css"
 
 import { AppProps } from "next/app"
 
-// TODO: Move to _document.tsx?
-//// import { Fira_Code, Inter } from "@next/font/google"
-
+import { Fira_Code, Inter } from "@next/font/google"
 import { Layout } from "../layout"
+import { detab } from "../lib/format"
 import { ShikiProvider } from "../shiki"
 import { StateProvider } from "../state"
 import { IconProps } from "./[icon]"
 
-//// // TODO
-//// const inter = Inter({
-//// 	subsets: ["latin"],
-//// 	variable: "--font-inter",
-//// })
-////
-//// // TODO
-//// const firaCode = Fira_Code({
-//// 	subsets: ["latin"],
-//// 	variable: "--font-fira-code",
-//// })
+const sans = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+})
+
+const code = Fira_Code({
+	subsets: ["latin"],
+	variable: "--font-fira-code",
+})
 
 export default function App({ pageProps }: AppProps<Partial<IconProps>>) {
 	const name = pageProps.name
 
 	return (
-		<ShikiProvider>
-			<StateProvider>
-				<Layout name={name} />
-			</StateProvider>
-		</ShikiProvider>
+		<>
+			{/* prettier-ignore */}
+			<style dangerouslySetInnerHTML={{__html: detab(`
+				:root, ::before, ::after {
+					--sans: ${sans.style.fontFamily};
+					--code: ${code.style.fontFamily};
+					font-family: var(--sans);
+				}
+				pre, code {
+					font-family: var(--code);
+					tab-size: 2;
+				}
+			`)}} />
+			<ShikiProvider>
+				<StateProvider>
+					<Layout name={name} />
+				</StateProvider>
+			</ShikiProvider>
+		</>
 	)
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,8 +24,8 @@ export default function Document() {
 			<Head />
 
 			{/* Google Fonts */}
-			<link rel="preconnect" href="https://fonts.googleapis.com" />
-			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+			{/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
+			{/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" /> */}
 			{/* <link href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" /> */}
 
 			{/* Plausible Analytics */}
