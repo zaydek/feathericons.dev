5ee06d2aafadcaf2439822b70c64141676820282
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2023-01-29 09:28:26 -0800

    Sponsor placeholders

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import "../css/uno.generated.css"
 import { Fira_Code, Inter } from "@next/font/google"
 import { AppProps } from "next/app"
 import { createElement, PropsWithChildren } from "react"
-import { cx } from "../lib/cx"
 import { detab } from "../lib/format"
 import { iota } from "../lib/iota"
 import { IconProps } from "./[icon]"
@@ -29,9 +28,9 @@ function BackgroundMask() {
 	return (
 		<div className="hide 2xl:show sticky t-0 z-[var(--mask-bg-z-index)]">
 			{/* Use h-0 here prevents masks from stacking */}
-			{/* Use overflow-x-[clip] here to prevent -mx-10% from side-scrolling */}
+			{/* Use overflow-x-[clip] here to prevent mar-x--10% from side-scrolling */}
 			<div className="h-0 overflow-x-[clip]">
-				<HeroBackgroundImage className="-mx-10% h-160 rounded-b-50%" />
+				<HeroBackgroundImage className="mar-x--10% h-160 rounded-b-50%" />
 			</div>
 		</div>
 	)
@@ -65,14 +64,40 @@ function ForegroundMask() {
 function StickyContainer({ pos, children }: PropsWithChildren<{ pos: "tl" | "tr" }>) {
 	return (
 		<div
-			// prettier-ignore
-			className={cx(`sticky t-0 2xl:t-[var(--inset-y)] z-[var(--card-z-index)]
-				${pos === "tl"
-					? "2xl:rounded-tl-[var(--app-rounding)]"
-					: "2xl:rounded-tr-[var(--app-rounding)]"}
-				bg-[#fff]`)}
+			// TODO: Do we really want bg- here?
+			className="sticky t-0 2xl:t-[var(--inset-y)] z-[var(--card-z-index)] bg-[#fff]
+				2xl:[&[data-pos=tr]]:rounded-tr-[var(--app-rounding)]
+				2xl:[&[data-pos=tl]]:rounded-tl-[var(--app-rounding)]"
+			data-pos={pos}
 		>
 			{children}
+		</div>
+	)
+}
+
+function SponsorMeta() {
+	return (
+		<div className="flex align-items-[center] gap-16">
+			<div className="flex align-items-[center] h-16">
+				<div className="h-6 aspect-16 rounded-1e3 bg-[#fff]"></div>
+			</div>
+			<div className="h-3 w-3 rounded-1e3 bg-[#fff]"></div>
+			<div className="flex align-items-[center] h-16">
+				<div className="h-6 aspect-16 rounded-1e3 bg-[#fff]"></div>
+			</div>
+		</div>
+	)
+}
+
+function SponsorSlot() {
+	return (
+		<div className="flex flex-col gap-8">
+			{/* Logo placeholder */}
+			<div className="h-[calc((48px_+_64px)_/_2)] aspect-3 rounded-1e3 bg-[#fff]"></div>
+			{/* Name placeholder */}
+			<div className="flex justify-content-[center] align-items-[center] h-16">
+				<div className="h-6 aspect-16 rounded-1e3 bg-[#fff]"></div>
+			</div>
 		</div>
 	)
 }
@@ -81,8 +106,23 @@ function Layout() {
 	return (
 		<>
 			{/* Header */}
-			<HeroBackgroundImage tag="header" className="h-320">
-				{/* TODO */}
+			<HeroBackgroundImage tag="header" className="flex justify-content-[center] pad-y-64">
+				<div className="flex align-items-[center] w-100% max-w-1024">
+					{/* LHS */}
+					<div className="flex flex-col flex-grow-[1]">
+						{/* <div className="h-48 aspect-3 rounded-1e3 bg-[#fff]"></div>
+						<div className="h-48 aspect-3 rounded-1e3 bg-[#fff]"></div> */}
+					</div>
+					{/* RHS */}
+					<div className="flex flex-col align-items-[center] gap-32 w-100% max-w-640">
+						<SponsorMeta />
+						<div className="flex justify-content-[center] flex-wrap-[wrap] gap-16">
+							{iota(5).map(index => (
+								<SponsorSlot key={index} />
+							))}
+						</div>
+					</div>
+				</div>
 			</HeroBackgroundImage>
 
 			{/* Masks */}
@@ -90,7 +130,7 @@ function Layout() {
 			<ForegroundMask />
 
 			{/* Card */}
-			<div className="flex justify-content-[center] pb-[calc(var(--inset-y)_*_2)]">
+			<div className="flex justify-content-[center] pad-b-[calc(var(--inset-y)_*_2)]">
 				<div className="flex w-100% max-w-1536 bg-[#fff] sh-[var(--shadow-3)] 2xl:rounded-[var(--app-rounding)]">
 					{/* LHS */}
 					{/* TODO: Use min-w max-w here? */}
