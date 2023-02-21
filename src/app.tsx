2c2b5e64656347f4af8b0f3ae1657604187a32e6
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2023-02-20 16:14:19 -0800

    Improve semantics

--- src/app.tsx
+++ src/app.tsx
@@ -6,12 +6,15 @@ import { IconComponent } from "@/components/dynamic-icon"
 import { CreativeCommons } from "@/components/license"
 import { CSSProperties, PropsWithChildren, useEffect, useRef, useState } from "react"
 
-function SectionName({ name, canUndo = true }: { name: string; canUndo?: boolean }) {
+////////////////////////////////////////////////////////////////////////////////
+// Sidebar
+
+function Legend({ name, canUndo = true }: { name: string; canUndo?: boolean }) {
 	return (
-		<div className="section-name">
-			<h2 className="section-name-type">{name}</h2>
-			{canUndo && <feather.RotateCcw className="section-name-undo-button" strokeWidth={4} />}
-		</div>
+		<legend>
+			<div className="legend-type">{name}</div>
+			{canUndo && <feather.RotateCcw className="legend-undo-button" strokeWidth={4} />}
+		</legend>
 	)
 }
 
@@ -19,7 +22,6 @@ function CheckboxItem({ name, icon: Icon }: { name: string; icon: IconComponent 
 	const [checked, setChecked] = useState(false)
 
 	return (
-		// TODO
 		<label className="checkbox-item">
 			<Icon className="checkbox-icon" />
 			<div className="checkbox-type">{name}</div>
@@ -74,10 +76,95 @@ function SearchBar() {
 	)
 }
 
+function Sidebar() {
+	const [didScroll, setDidScroll] = useState(false)
+
+	return (
+		<aside className="sidebar">
+			<header className="sticky-search-bar-container" data-state-did-scroll={didScroll}>
+				<SearchBar />
+			</header>
+			<div
+				className="scrolling-container"
+				onScroll={e => {
+					setDidScroll(e.currentTarget.scrollTop > 0)
+				}}
+			>
+				<fieldset>
+					<ul className="checkbox-list">
+						<CheckboxItem name="Feather" icon={feather.Feather} />
+					</ul>
+					<ul className="checkbox-list">
+						<CheckboxItem name="Feather" icon={feather.ChevronDown} />
+						<ul className="checkbox-list">
+							<CheckboxItem name="Feather" icon={wkSocial.Twitter} />
+							<ul className="checkbox-list">
+								<CheckboxItem name="Feather" icon={wkSocial.TwitterCircle} />
+								<CheckboxItem name="Feather" icon={wkSocial.TwitterSquare} />
+							</ul>
+						</ul>
+					</ul>
+					<ul className="checkbox-list">
+						<CheckboxItem name="Feather" icon={feather.ChevronDown} />
+						<ul className="checkbox-list">
+							<CheckboxItem name="Feather" icon={wkVendor.Stripe} />
+							<ul className="checkbox-list">
+								<CheckboxItem name="Feather" icon={wkVendor.Stripe1} />
+								<CheckboxItem name="Feather" icon={wkVendor.Stripe2} />
+							</ul>
+						</ul>
+					</ul>
+				</fieldset>
+				<hr />
+				<fieldset>
+					<Legend name="Preview size" />
+					<Slider />
+				</fieldset>
+				<hr />
+				<fieldset>
+					<Legend name="Preview stroke" />
+					<Slider />
+				</fieldset>
+				<hr />
+				<fieldset>
+					<Legend name="Licenses" canUndo={false} />
+					<ul className="license-list">
+						{/* prettier-ignore */}
+						<LicenseItem icon={feather.Feather}>
+								<a href="TODO">Feather icons</a> designed by <a href="TODO">@colebemis</a><br />
+								Licensed as MIT<br />
+								Personal & commercial use OK <em>without</em> attribution<br />
+							</LicenseItem>
+						{/* prettier-ignore */}
+						<LicenseItem icon={CreativeCommons}>
+								<a href="TODO">Social icons</a> sourced by <a href="TODO">The Wolf Kit</a><br />
+								Licensed as CC BY 4.0<br />
+								Personal & commercial use OK <em>with</em> attribution<br />
+							</LicenseItem>
+						{/* prettier-ignore */}
+						<LicenseItem icon={CreativeCommons}>
+								<a href="TODO">Vendor icons</a> sourced by <a href="TODO">The Wolf Kit</a><br />
+								Licensed as CC BY 4.0<br />
+								Personal & commercial use OK <em>with</em> attribution<br />
+							</LicenseItem>
+					</ul>
+				</fieldset>
+			</div>
+		</aside>
+	)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Body
+
+function Body() {
+	return <main className="body">{/* ... */}</main>
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 export function App() {
 	const [showOutline, setShowOutline] = useState(false)
-
-	const [didScroll, setDidScroll] = useState(false)
 
 	useEffect(() => {
 		function handleKeyDown(e: KeyboardEvent) {
@@ -92,79 +179,8 @@ export function App() {
 	return (
 		<>
 			{showOutline && <style>{`*:not(svg *) { outline: 1px solid hsl(0, 100%, 50%, 0.125); }`}</style>}
-			<aside className="sidebar">
-				<header className="sticky-search-bar-container" data-state-did-scroll={didScroll}>
-					<SearchBar />
-				</header>
-				<div
-					className="scrolling-container"
-					onScroll={e => {
-						setDidScroll(e.currentTarget.scrollTop > 0)
-					}}
-				>
-					<section>
-						{/* <SectionName name="Icons" /> */}
-						<ul className="checkbox-list">
-							<CheckboxItem name="Feather" icon={feather.Feather} />
-						</ul>
-						<ul className="checkbox-list">
-							<CheckboxItem name="Feather" icon={feather.ChevronDown} />
-							<ul className="checkbox-list">
-								<CheckboxItem name="Feather" icon={wkSocial.Twitter} />
-								<ul className="checkbox-list">
-									<CheckboxItem name="Feather" icon={wkSocial.TwitterCircle} />
-									<CheckboxItem name="Feather" icon={wkSocial.TwitterSquare} />
-								</ul>
-							</ul>
-						</ul>
-						<ul className="checkbox-list">
-							<CheckboxItem name="Feather" icon={feather.ChevronDown} />
-							<ul className="checkbox-list">
-								<CheckboxItem name="Feather" icon={wkVendor.Stripe} />
-								<ul className="checkbox-list">
-									<CheckboxItem name="Feather" icon={wkVendor.Stripe1} />
-									<CheckboxItem name="Feather" icon={wkVendor.Stripe2} />
-								</ul>
-							</ul>
-						</ul>
-					</section>
-					<hr />
-					<section>
-						<SectionName name="Preview size" />
-						<Slider />
-					</section>
-					<hr />
-					<section>
-						<SectionName name="Preview stroke" />
-						<Slider />
-					</section>
-					<hr />
-					<section>
-						<SectionName name="Licenses" canUndo={false} />
-						<ul className="license-list">
-							{/* prettier-ignore */}
-							<LicenseItem icon={feather.Feather}>
-								<a href="TODO">Feather icons</a> designed by <a href="TODO">@colebemis</a><br />
-								Licensed as MIT<br />
-								Personal & commercial use OK <em>without</em> attribution<br />
-							</LicenseItem>
-							{/* prettier-ignore */}
-							<LicenseItem icon={CreativeCommons}>
-								<a href="TODO">Social icons</a> sourced by <a href="TODO">The Wolf Kit</a><br />
-								Licensed as CC BY 4.0<br />
-								Personal & commercial use OK <em>with</em> attribution<br />
-							</LicenseItem>
-							{/* prettier-ignore */}
-							<LicenseItem icon={CreativeCommons}>
-								<a href="TODO">Vendor icons</a> sourced by <a href="TODO">The Wolf Kit</a><br />
-								Licensed as CC BY 4.0<br />
-								Personal & commercial use OK <em>with</em> attribution<br />
-							</LicenseItem>
-						</ul>
-					</section>
-				</div>
-			</aside>
-			<main className="body">{/* ... */}</main>
+			<Sidebar />
+			<Body />
 		</>
 	)
 }
