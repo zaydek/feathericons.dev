5b2308ff7acbc6a3c51a07661efa514f12401e29
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2023-02-26 05:52:41 -0800

    Cleaner grid, variable Inter font

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -15,27 +15,20 @@ export function GridItem({
 	selected?: boolean
 }) {
 	return (
-		<li className="grid-item" data-bookmark={bookmark} data-selected={selected}>
-			<div className="icon-frame u-flex-1">
-				<Icon className="icon" />
-			</div>
-			{/* <div className="name">
-				<Icon className="icon" strokeWidth={2.5} />
-				&ensp;{name}
-			</div> */}
-			<div className="name">{name}</div>
-			<div className="buttons">
-				<div className="button u-flex-1">
-					<div className="type">COPY SVG</div>
-				</div>
-				<div className="button">
-					<feather.ChevronDown className="icon" strokeWidth={6} />
-				</div>
-			</div>
-			{/* <div className="copy-button">
-				<div className="type">COPY</div>
-			</div> */}
-			<feather.Star className="bookmark" fill="currentColor" strokeWidth={6} />
+		<li data-bookmark={bookmark} data-selected={selected}>
+			<article>
+				<figure className="u-flex-1">
+					<Icon className="icon" strokeWidth={2.5} />
+				</figure>
+				<figcaption>
+					{/* <Icon className="icon" strokeWidth={2.5} />
+					&ensp; */}
+					{/* {name} */}
+					{/* Hello hello hello hello hello hello hello */}
+					{name}
+				</figcaption>
+				<feather.Star className="bookmark" fill="currentColor" strokeWidth={6} />
+			</article>
 		</li>
 	)
 }
