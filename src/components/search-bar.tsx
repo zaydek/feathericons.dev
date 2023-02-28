1acd9e9ae84e5c72719fd476faa3c974f0127c2f
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2023-02-27 20:19:46 -0800

    Add placeholder for search bar on focus

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -4,22 +4,31 @@ import * as feather from "@icons/feather"
 
 import { isMac } from "@/lib"
 import { SearchContext } from "@/state"
-import { useContext, useEffect, useRef } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 
 export function SearchBar() {
 	const ref = useRef<HTMLInputElement | null>(null)
 	const { search, setSearch } = useContext(SearchContext)!
 
+	const [hasFocus, setHasFocus] = useState(false)
+
 	useEffect(() => {
 		function handleKeyDown(e: KeyboardEvent) {
 			if ((isMac() ? e.metaKey : e.ctrlKey) && e.key === "p") {
 				e.preventDefault() // Printers 💀
+				//// if (document.activeElement === ref.current) {
+				//// 	ref.current!.blur()
+				//// } else {
+				//// 	ref.current!.focus()
+				//// }
 				ref.current!.focus()
+			} else if (e.key === "Escape") {
+				setSearch("")
 			}
 		}
 		window.addEventListener("keydown", handleKeyDown, false)
 		return () => window.removeEventListener("keydown", handleKeyDown, false)
-	}, [])
+	}, [setSearch])
 
 	return (
 		<div className="search-bar">
@@ -27,8 +36,14 @@ export function SearchBar() {
 			<input
 				ref={ref}
 				type="text"
-				placeholder={isMac() ? "Press ⌘P to Focus" : "Press Ctrl-P to Focus"}
+				// prettier-ignore
+				placeholder={hasFocus
+					? "Press esc to clear"
+					: (isMac() ? "Press ⌘P to focus" : "Press Ctrl-P to focus")
+				}
 				value={search}
+				onFocus={e => setHasFocus(true)}
+				onBlur={e => setHasFocus(false)}
 				onChange={e => setSearch(e.currentTarget.value)}
 			/>
 		</div>
