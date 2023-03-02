72d0e9179af696ca9f86be1a19b3e9cb5c38c415
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2023-03-01 20:52:37 -0800

    Some tweaks

--- src/components/syntax-highlighting.tsx
+++ src/components/syntax-highlighting.tsx
@@ -4,6 +4,46 @@ import { ShikiContext } from "@/state"
 import { useContext, useEffect, useState } from "react"
 import { IThemedToken, Lang } from "shiki-es"
 
+//// export function SyntaxHighlighting({ lang, code }: { lang: Lang; code: string }) {
+//// 	const { highlighter } = useContext(ShikiContext)!
+////
+//// 	const [tokens, setTokens] = useState<IThemedToken[][] | null>(null)
+////
+//// 	useEffect(() => {
+//// 		if (highlighter === null) return
+//// 		const tokens = highlighter.codeToThemedTokens(code, lang, "github-light")
+//// 		setTokens(tokens)
+//// 	}, [code, highlighter, lang])
+////
+//// 	return (
+//// 		<pre className="pre">
+//// 			<code className="pre-code">
+//// 				{tokens === null
+//// 					? // No syntax highlighting
+//// 					  code.split("\n").map((ys, y) => (
+//// 							<div key={y}>
+//// 								{ys || <br />}
+//// 							</div>
+//// 					  ))
+//// 					: // Syntax highlighting
+//// 					  tokens.map((ys, y) => (
+//// 							<div key={y}>
+//// 								{ys.length > 0 ? (
+//// 									ys.map(({ color, content }, x) => (
+//// 										<span key={x} style={{ color }}>
+//// 											{content}
+//// 										</span>
+//// 									))
+//// 								) : (
+//// 									<br />
+//// 								)}
+//// 							</div>
+//// 					  ))}
+//// 			</code>
+//// 		</pre>
+//// 	)
+//// }
+
 export function SyntaxHighlighting({ lang, code }: { lang: Lang; code: string }) {
 	const { highlighter } = useContext(ShikiContext)!
 
@@ -19,15 +59,9 @@ export function SyntaxHighlighting({ lang, code }: { lang: Lang; code: string })
 		<pre className="pre">
 			<code className="pre-code">
 				{tokens === null
-					? // No syntax highlighting
-					  code.split("\n").map((ys, y) => (
-							<div key={y} className="line">
-								{ys || <br />}
-							</div>
-					  ))
-					: // Syntax highlighting
-					  tokens.map((ys, y) => (
-							<div key={y} className="line">
+					? code.split("\n").map((ys, y) => <div key={y}>{ys || <br />}</div>)
+					: tokens.map((ys, y) => (
+							<div key={y}>
 								{ys.length > 0 ? (
 									ys.map(({ color, content }, x) => (
 										<span key={x} style={{ color }}>
