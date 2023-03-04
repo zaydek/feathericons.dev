dfdc1ecbf2b0fe0b538d329450f832fe54997014
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2023-03-03 19:00:52 -0800

    Remove the need for original section

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,14 @@
 import * as feather from "@icons/feather/tsx"
-import * as wolfKitBrandsMonoCircle from "@icons/wolfkit/brands/mono-circle/tsx"
-import * as wolfKitBrandsMonoSquare from "@icons/wolfkit/brands/mono-square/tsx"
-import * as wolfKitBrandsMono from "@icons/wolfkit/brands/mono/tsx"
-import * as wolfKitBrandsOriginalCircle from "@icons/wolfkit/brands/original-circle/tsx"
-import * as wolfKitBrandsOriginalSquare from "@icons/wolfkit/brands/original-square/tsx"
-import * as wolfKitBrandsOriginal from "@icons/wolfkit/brands/original/tsx"
-import * as wolfKitPaymentsMonoFilled from "@icons/wolfkit/payments/mono-filled/tsx"
-import * as wolfKitPaymentsMono from "@icons/wolfkit/payments/mono/tsx"
-import * as wolfKitPaymentsOriginalFilled from "@icons/wolfkit/payments/original-filled/tsx"
-import * as wolfKitPaymentsOriginal from "@icons/wolfkit/payments/original/tsx"
+import * as wkBrandsMonoCircle from "@icons/wolfkit/brands/mono-circle/tsx"
+import * as wkBrandsMonoSquare from "@icons/wolfkit/brands/mono-square/tsx"
+import * as wkBrandsMono from "@icons/wolfkit/brands/mono/tsx"
+import * as wkBrandsOriginalCircle from "@icons/wolfkit/brands/original-circle/tsx"
+import * as wkBrandsOriginalSquare from "@icons/wolfkit/brands/original-square/tsx"
+import * as wkBrandsOriginal from "@icons/wolfkit/brands/original/tsx"
+import * as wkPaymentsMonoFilled from "@icons/wolfkit/payments/mono-filled/tsx"
+import * as wkPaymentsMono from "@icons/wolfkit/payments/mono/tsx"
+import * as wkPaymentsOriginalFilled from "@icons/wolfkit/payments/original-filled/tsx"
+import * as wkPaymentsOriginal from "@icons/wolfkit/payments/original/tsx"
 
 import {
 	Checkbox,
@@ -17,21 +17,15 @@ import {
 	Main,
 	MemoGrid,
 	MonochromeCheckboxFolder,
-	NoNameSection,
 	Range,
 	Resource,
-	Resources,
 	SearchBar,
-	Section,
 	SelectExportAs,
 	Sidebar1,
 	Sidebar2,
-	SidebarBody,
-	SidebarFooter,
-	SidebarHeader,
 	SyntaxHighlighting,
-	UndoSection,
 } from "@/components"
+import { useScrollProps } from "@/hooks"
 import { useVisibleDocumentTitle } from "@/hooks/document-title"
 import {
 	ClipboardContext,
@@ -47,7 +41,6 @@ import {
 } from "@/state"
 import { useCallback, useContext, useEffect, useMemo, useTransition } from "react"
 import { Lang } from "shiki-es"
-import { useScrollProps } from "./hooks"
 
 export function App() {
 	const { setStarted } = useContext(ProgressBarContext)!
@@ -68,8 +61,6 @@ export function App() {
 }
 
 function AppSidebar1() {
-	const { setStarted } = useContext(ProgressBarContext)!
-
 	const {
 		showFeather,
 		setShowFeather,
@@ -91,7 +82,15 @@ function AppSidebar1() {
 		resetWolfKit,
 	} = useContext(SearchContext)!
 
+	const { scrollProps } = useScrollProps()
+
+	const { setStarted } = useContext(ProgressBarContext)!
 	const [pending, startTransition] = useTransition()
+
+	const setAllMonochrome = useCallback(() => {
+		setBrandsMonochrome(true)
+		setPaymentsMonochrome(true)
+	}, [setBrandsMonochrome, setPaymentsMonochrome])
 
 	const createVoidTransition = useCallback(function (fn: () => void) {
 		return () => startTransition(fn)
@@ -109,113 +108,95 @@ function AppSidebar1() {
 
 	return (
 		<Sidebar1>
-			<SidebarHeader>
-				<NoNameSection>
+			<header className="section-2-header">
+				<section className="section-2 is-start">
 					<SearchBar />
-				</NoNameSection>
-			</SidebarHeader>
-			<SidebarBody>
-				<UndoSection
-					name="Icons"
-					icon={feather.Package}
-					handleUndo={createVoidTransition(() => {
-						resetFeather()
-						resetWolfKit()
-					})}
-				>
-					{/* <div> */}
-					<Checkboxes>
-						<Checkbox
-							name="Feather"
-							icon={feather.Feather}
-							checked={showFeather}
-							setChecked={createTransition(setShowFeather)}
-						/>
-					</Checkboxes>
-					<Checkboxes>
-						<MonochromeCheckboxFolder
-							name={brandsMonochrome ? "Brands (mono)" : "Brands"}
-							icon={p => (
-								<feather.Shield
-									//// style={{ transform: "scale(0.8)", opacity: 0.375 }}
-									//// fill="currentColor"
-									//// strokeWidth={6}
-									{...p}
-								/>
-							)}
-							checked={brandsMonochrome}
-							setChecked={createVoidTransition(() => {
-								setBrandsMonochrome(curr => !curr)
-								setPaymentsMonochrome(curr => !curr)
-							})}
-						/>
+				</section>
+				<div className="section-2-header-body" {...scrollProps}>
+					<section className="section-2">
+						<header className="section-2-header-header">
+							<feather.Package className="section-2-icon" />
+							<h6 className="section-header-name u-flex-1">Icons</h6>
+							<feather.RotateCcw className="section-2-header-undo" strokeWidth={4} />
+						</header>
 						<Checkboxes>
 							<Checkbox
-								name="Original"
-								icon={brandsMonochrome ? wolfKitBrandsMono.Twitter : wolfKitBrandsOriginal.Twitter}
-								checked={showBrandsOriginal}
-								setChecked={createTransition(setShowBrandsOriginal)}
-							/>
-							<Checkbox
-								name="Circle"
-								icon={brandsMonochrome ? wolfKitBrandsMonoCircle.Twitter : wolfKitBrandsOriginalCircle.Twitter}
-								checked={showBrandsOriginalCircle}
-								setChecked={createTransition(setShowBrandsOriginalCircle)}
-							/>
-							<Checkbox
-								name="Square"
-								icon={brandsMonochrome ? wolfKitBrandsMonoSquare.Twitter : wolfKitBrandsOriginalSquare.Twitter}
-								checked={showBrandsOriginalSquare}
-								setChecked={createTransition(setShowBrandsOriginalSquare)}
+								name="Feather"
+								icon={feather.Feather}
+								checked={showFeather}
+								setChecked={createTransition(setShowFeather)}
 							/>
 						</Checkboxes>
-					</Checkboxes>
-					<Checkboxes>
-						<MonochromeCheckboxFolder
-							name={paymentsMonochrome ? "Payments (mono)" : "Payments"}
-							icon={p => (
-								<feather.CreditCard
-									//// style={{ transform: "scale(0.8)", opacity: 0.375 }}
-									//// fill="currentColor"
-									//// strokeWidth={6}
-									{...p}
-								/>
-							)}
-							checked={paymentsMonochrome}
-							setChecked={createVoidTransition(() => {
-								setPaymentsMonochrome(curr => !curr)
-								setBrandsMonochrome(curr => !curr)
-							})}
-						/>
 						<Checkboxes>
-							<Checkbox
-								name="Original"
-								icon={paymentsMonochrome ? wolfKitPaymentsMono.Stripe : wolfKitPaymentsOriginal.Stripe}
-								checked={showPaymentsOriginal}
-								setChecked={createTransition(setShowPaymentsOriginal)}
+							<MonochromeCheckboxFolder
+								name={brandsMonochrome ? "Brands (mono)" : "Brands"}
+								icon={feather.Shield}
+								checked={brandsMonochrome}
+								setChecked={createVoidTransition(setAllMonochrome)}
 							/>
-							<Checkbox
-								name="Filled"
-								icon={paymentsMonochrome ? wolfKitPaymentsMonoFilled.Stripe : wolfKitPaymentsOriginalFilled.Stripe}
-								checked={showPaymentsOriginalFilled}
-								setChecked={createTransition(setShowPaymentsOriginalFilled)}
-							/>
+							<Checkboxes>
+								<Checkbox
+									name="Original"
+									icon={brandsMonochrome ? wkBrandsMono.Twitter : wkBrandsOriginal.Twitter}
+									checked={showBrandsOriginal}
+									setChecked={createTransition(setShowBrandsOriginal)}
+								/>
+								<Checkbox
+									name="Circle"
+									icon={brandsMonochrome ? wkBrandsMonoCircle.Twitter : wkBrandsOriginalCircle.Twitter}
+									checked={showBrandsOriginalCircle}
+									setChecked={createTransition(setShowBrandsOriginalCircle)}
+								/>
+								<Checkbox
+									name="Square"
+									icon={brandsMonochrome ? wkBrandsMonoSquare.Twitter : wkBrandsOriginalSquare.Twitter}
+									checked={showBrandsOriginalSquare}
+									setChecked={createTransition(setShowBrandsOriginalSquare)}
+								/>
+							</Checkboxes>
 						</Checkboxes>
-					</Checkboxes>
-					{/* </div> */}
-				</UndoSection>
-				<hr className="hr" />
-			</SidebarBody>
-			<SidebarFooter>
-				<Section name="Resources" icon={feather.Globe}>
-					<Resources>
-						<Resource name="Icons" icon={wolfKitBrandsOriginal.Github} />
-						<Resource name="Website" icon={wolfKitBrandsOriginal.Github} />
-						<Resource name="Figma Plugin" icon={wolfKitBrandsOriginal.Figma} />
-						<Resource name="Share on Twitter" icon={wolfKitBrandsOriginal.Twitter} />
-					</Resources>
-				</Section>
-			</SidebarFooter>
+						<Checkboxes>
+							<MonochromeCheckboxFolder
+								name={paymentsMonochrome ? "Payments (mono)" : "Payments"}
+								icon={feather.CreditCard}
+								checked={paymentsMonochrome}
+								setChecked={createVoidTransition(setAllMonochrome)}
+							/>
+							<Checkboxes>
+								<Checkbox
+									name="Original"
+									icon={paymentsMonochrome ? wkPaymentsMono.Stripe : wkPaymentsOriginal.Stripe}
+									checked={showPaymentsOriginal}
+									setChecked={createTransition(setShowPaymentsOriginal)}
+								/>
+								<Checkbox
+									name="Filled"
+									icon={paymentsMonochrome ? wkPaymentsMonoFilled.Stripe : wkPaymentsOriginalFilled.Stripe}
+									checked={showPaymentsOriginalFilled}
+									setChecked={createTransition(setShowPaymentsOriginalFilled)}
+								/>
+							</Checkboxes>
+						</Checkboxes>
+					</section>
+					<hr className="hr" />
+				</div>
+			</header>
+			<div className="section-2-spacer"></div>
+			<footer className="section-2-footer">
+				<hr className="hr is-collapsible" />
+				<section className="section-2 is-end">
+					<header className="section-2-header-header">
+						<feather.Globe className="section-2-icon" />
+						<h6 className="section-header-name u-flex-1">Resources</h6>
+					</header>
+					<nav className="resources">
+						<Resource name="Icons" icon={wkBrandsOriginal.Github} />
+						<Resource name="Website" icon={wkBrandsOriginal.Github} />
+						<Resource name="Figma Plugin" icon={wkBrandsOriginal.Figma} />
+						<Resource name="Share on Twitter" icon={wkBrandsOriginal.Twitter} />
+					</nav>
+				</section>
+			</footer>
 		</Sidebar1>
 	)
 }
@@ -226,7 +207,6 @@ function AppSidebar2() {
 
 	const { scrollProps } = useScrollProps()
 
-	//// const $size = selected.size
 	const lang: Lang = exportAs === "svg" ? "html" : "tsx"
 
 	return (
@@ -239,18 +219,18 @@ function AppSidebar2() {
 						<SelectExportAs value={exportAs} setValue={setExportAs} />
 					</header>
 				</section>
-				<div className="section-2-header-body" {...scrollProps}>
+				<div className="section-2-header-body is-padding-bottom" {...scrollProps}>
 					<SyntaxHighlighting lang={lang} code={clipboard} />
 				</div>
 			</header>
 			<div className="section-2-body">
-				{/* <hr className="hr" />
+				<hr className="hr" />
 				<section className="section-2">
 					<header className="section-2-header-header">
 						<feather.Circle className="section-2-icon" />
 						<h6 className="section-header-name u-flex-1">Color</h6>
 					</header>
-				</section> */}
+				</section>
 				<hr className="hr" />
 				<section className="section-2">
 					<header className="section-2-header-header">
@@ -285,56 +265,6 @@ function AppSidebar2() {
 			</footer>
 		</Sidebar2>
 	)
-
-	//// return (
-	//// 	<Sidebar2>
-	//// 		<SidebarHeader>
-	//// 			<SelectSection<ExportAs>
-	//// 				/// name={$size < 2 ? "Selected" : `Selected\u2002\u00B7\u2002${$size} icons`}
-	//// 				//// name={$size > 1 ? `Selected ${$size} icons` : "Selected"}
-	//// 				name="Selected"
-	//// 				icon={feather.MousePointer}
-	//// 				value={exportAs}
-	//// 				setValue={setExportAs}
-	//// 			>
-	//// 				<SyntaxHighlighting lang={lang} code={clipboard} />
-	//// 			</SelectSection>
-	//// 		</SidebarHeader>
-	//// 		<SidebarBody>
-	//// 			{/* <hr className="hr" />
-	//// 			<SliderUndoSection
-	//// 				name="Color"
-	//// 				icon={feather.Image}
-	//// 				value={size}
-	//// 				formatValue={value => `${value.toFixed(0)} PX`}
-	//// 				handleUndo={resetSize}
-	//// 			>
-	//// 				<Range value={size} setValue={setSize} min={SIZE_MIN} max={SIZE_MAX} step={SIZE_STEP} />
-	//// 			</SliderUndoSection> */}
-	//// 			<hr className="hr" />
-	//// 			<SliderUndoSection
-	//// 				name="Preview size"
-	//// 				icon={feather.PenTool}
-	//// 				value={size}
-	//// 				formatValue={value => `${value.toFixed(0)} PX`}
-	//// 				handleUndo={resetSize}
-	//// 			>
-	//// 				<Range value={size} setValue={setSize} min={SIZE_MIN} max={SIZE_MAX} step={SIZE_STEP} />
-	//// 			</SliderUndoSection>
-	//// 			<hr className="hr" />
-	//// 			<SliderUndoSection
-	//// 				name="Preview stroke width"
-	//// 				icon={feather.PenTool}
-	//// 				value={strokeWidth}
-	//// 				formatValue={value => value.toFixed(2)}
-	//// 				handleUndo={resetStrokeWidth}
-	//// 			>
-	//// 				<Range value={strokeWidth} setValue={setStrokeWidth} min={STROKE_MIN} max={STROKE_MAX} step={STROKE_STEP} />
-	//// 			</SliderUndoSection>
-	//// 			<hr className="hr" />
-	//// 		</SidebarBody>
-	//// 	</Sidebar2>
-	//// )
 }
 
 function AppMain() {
--- src/components/resources.tsx
+++ src/components/resources.tsx
@@ -3,11 +3,10 @@ import "./resources.sass"
 import * as feather from "@icons/feather/tsx"
 
 import { Icon } from "@/lib"
-import { PropsWithChildren } from "react"
 
-export function Resources({ children }: PropsWithChildren) {
-	return <nav className="resources">{children}</nav>
-}
+//// export function Resources({ children }: PropsWithChildren) {
+//// 	return <nav className="resources">{children}</nav>
+//// }
 
 export function Resource({ name, icon: Icon }: { name: string; icon: Icon }) {
 	return (
