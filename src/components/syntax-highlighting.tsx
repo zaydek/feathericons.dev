41fc7a88c44b4103ea93c45dbd7a0d75217c747c
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2023-03-06 00:53:41 -0800

    Added fallback for syntax highlighting

--- src/components/syntax-highlighting.tsx
+++ src/components/syntax-highlighting.tsx
@@ -3,13 +3,11 @@ import { ShikiContext } from "@/state"
 import { Fragment, useContext, useEffect, useState } from "react"
 import { IThemedToken } from "shiki-es"
 
-// prettier-ignore
 function TwitterLink({ username, children }: { username: string; children: string }) {
 	const href = `https://twitter.com/${username}`
 	return <Anchor href={href}>{children}</Anchor>
 }
 
-// prettier-ignore
 function Link({ href, children }: { href: string; children: string }) {
 	return <Anchor href={href}>{children}</Anchor>
 }
@@ -21,10 +19,10 @@ function RenderLink({ children }: { children: string }) {
 		: <Link href={children}>{children}</Link>
 }
 
-const regex = /(.*)(?!http:\/\/www\.w3\.org\/2000\/svg)(@[^\s]+|https?:\/\/[^\s]+)(.*)/g
+const urlRegex = /(.*)(?!http:\/\/www\.w3\.org\/2000\/svg)(@[^\s]+|https?:\/\/[^\s]+)(.*)/g
 
-function Token({ color, children }: { color?: string; children: string }) {
-	const arr = [...children.matchAll(regex)]
+function Tokenize({ color, children }: { color?: string; children: string }) {
+	const arr = [...children.matchAll(urlRegex)]
 	return (
 		<span style={{ color }}>
 			{arr.length > 0
@@ -46,7 +44,7 @@ export function SyntaxHighlighting({ lang, code }: { lang: string; code: string 
 
 	useEffect(() => {
 		if (highlighter === null) return
-		// Defer to JavaScript because of // comments
+		// NOTE: Defer to JavaScript because of // comments
 		const tokens = highlighter.codeToThemedTokens(code, code.startsWith("//") ? "js" : lang, "github-light")
 		setTokens(tokens)
 	}, [code, highlighter, lang])
@@ -54,25 +52,33 @@ export function SyntaxHighlighting({ lang, code }: { lang: string; code: string 
 	return (
 		<pre className="syntax-highlighting">
 			<code className="syntax-highlighting-code">
-				{tokens === null ? (
-					"TODO"
-				) : (
-					<>
-						{tokens.map((lineTokens, index) => (
-							<div key={index}>
-								{lineTokens.length > 0 ? (
-									lineTokens.map((token, index) => (
-										<Token key={index} color={token.color}>
-											{token.content}
-										</Token>
-									))
-								) : (
-									<br />
-								)}
-							</div>
-						))}
-					</>
-				)}
+				{tokens === null
+					? // No syntax highlighting
+					  // prettier-ignore
+					  code.split("\n").map((line, index) => (
+						<div key={index}>
+							{line.length > 0 ? (
+								<Tokenize>{line}</Tokenize>
+							) : (
+								<br />
+							)}
+						</div>
+					))
+					: // Syntax highlighting
+					  // prettier-ignore
+					  tokens.map((lineTokens, index) => (
+						<div key={index}>
+							{lineTokens.length > 0 ? (
+								lineTokens.map((token, index) => (
+									<Tokenize key={index} color={token.color}>
+										{token.content}
+									</Tokenize>
+								))
+							) : (
+								<br />
+							)}
+						</div>
+					))}
 			</code>
 		</pre>
 	)
