d38e4f1c97663747bfc22f05d902260abe9e7bcc
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2023-03-12 21:03:14 -0700

    State machine working

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react"
 
-import { PropsWithChildren } from "react";
-import { iota } from "./lib";
+import { iota, round } from "@/lib"
 
 function SidebarOverlay() {
 	return <div className="sidebar-overlay"></div>
@@ -11,71 +10,99 @@ function Sidebar1({
 	header,
 	footer,
 	children,
-}: PropsWithChildren<{ header: React.ReactNode; footer: React.ReactNode }>) {
-	const dragRef = React.useRef<HTMLDivElement | null>(null)
+}: React.PropsWithChildren<{ header: React.ReactNode; footer: React.ReactNode }>) {
+	const ref = React.useRef<HTMLDivElement | null>(null)
+	const dragAreaRef = React.useRef<HTMLDivElement | null>(null)
 
-	const [pointerIsDown, setPointerIsDown] = React.useState<boolean>(false)
+	const [state, setState] = React.useState<"maximized" | "minimized" | null>(null)
+
+	const [pointerDown, setPointerDown] = React.useState<boolean>(false)
 	const [startClientX, setStartClientX] = React.useState<number | null>(null)
 	const [clientX, setClientX] = React.useState<number | null>(null)
 
-	const translateX =
-		startClientX === null || clientX === null
-			? "translateX(-128px)"
-			: `translateX(calc(-128px + ${clientX - startClientX}px))`
+	const x = startClientX === null || clientX === null ? 0 : clientX - startClientX
 
-	useEffect(() => {
+	React.useEffect(() => {
 		function handlePointerDown(e: PointerEvent) {
-			if (e.button !== 0 && e.button !== 1) { return } // Guard mouse buttons
+			if (e.button !== 0 && e.button !== 1) return
+			// prettier-ignore
 			if (
-				dragRef.current !== null && e.target instanceof HTMLDivElement &&
-				!dragRef.current.contains(e.target)
-			) { return }
+				dragAreaRef.current !== null && e.target instanceof HTMLDivElement &&
+				!dragAreaRef.current.contains(e.target)) return
+
 			e.preventDefault() // Prevent text selection
-			setPointerIsDown(true)
-			setStartClientX(e.clientX)
-			setClientX(e.clientX)
+			setPointerDown(true)
+			setStartClientX(round(e.clientX, { precision: 4 }))
+			setClientX(round(e.clientX, { precision: 4 }))
 		}
 		function handlePointerMove(e: PointerEvent) {
-			if (!pointerIsDown) return
+			if (!pointerDown) return
 			setClientX(e.clientX)
 		}
 		function handlePointerUp(e: PointerEvent) {
-			setPointerIsDown(false)
+			const w = parseInt(window.getComputedStyle(ref.current!).getPropertyValue("--width"))
+			const maxw = parseInt(window.getComputedStyle(ref.current!).getPropertyValue("--maximized-width"))
+			const d = maxw - w
+			//// if (start) {
+			if (state === null) {
+				if (x > 0) {
+					setState("maximized")
+				} else if (x < 0) {
+					setState("minimized")
+				}
+			} else if (state === "maximized") {
+				if (x < -d) {
+					setState("minimized")
+				} else if (x < 0) {
+					setState(null)
+				}
+			} else if (state === "minimized") {
+				if (x > w) {
+					setState("maximized")
+				} else if (x > 0) {
+					setState(null)
+				}
+			}
+			//// }
+			setPointerDown(false)
 			setStartClientX(null)
 			setClientX(null)
 		}
 		document.addEventListener("pointerdown", handlePointerDown, false)
 		document.addEventListener("pointermove", handlePointerMove, false)
-		document.addEventListener("pointerup", handlePointerUp, false) // prettier-ignore
+		document.addEventListener("pointerup",   handlePointerUp,   false) // prettier-ignore
 		return () => {
 			document.removeEventListener("pointerdown", handlePointerDown, false)
 			document.removeEventListener("pointermove", handlePointerMove, false)
-			document.removeEventListener("pointerup", handlePointerUp, false) // prettier-ignore
+			document.removeEventListener("pointerup",   handlePointerUp,   false) // prettier-ignore
 		}
 	})
 
 	return (
 		<aside
-			className="sidebar-1"
-			style={{
-				// X-axis dragging
-				touchAction: "pan-x",
-				transform: translateX,
-			}}
+			ref={ref}
+			// Styling
+			className="sidebar"
+			data-pos="start"
+			data-state={state}
+			data-pointer-down={pointerDown || undefined} // Serialize
+			style={{ "--x": `${x}px` } as React.CSSProperties}
 		>
-			<div ref={dragRef} className="sidebar-1-drag-area">
-				<div className="sidebar-drag-handle"></div>
+			<div ref={dragAreaRef} className="sidebar-drag-area">
+				<div className="sidebar-drag-area-handle"></div>
 			</div>
-			<div className="sidebar-1-card">
-				<header className="sidebar-header">
-					{header}
-					<hr className="collapse" />
-				</header>
-				<div className="sidebar-scroll-area">{children}</div>
-				<footer className="sidebar-footer">
-					<hr className="collapse" />
-					{footer}
-				</footer>
+			<div className="sidebar-card">
+				<div className="sidebar-card-content">
+					<header className="sidebar-header">
+						{header}
+						<hr className="collapse" />
+					</header>
+					<div className="sidebar-scroll-area">{children}</div>
+					<footer className="sidebar-footer">
+						<hr className="collapse" />
+						{footer}
+					</footer>
+				</div>
 			</div>
 		</aside>
 	)
@@ -85,28 +112,28 @@ function Sidebar2({
 	header,
 	footer,
 	children,
-}: PropsWithChildren<{ header: React.ReactNode; footer: React.ReactNode }>) {
+}: React.PropsWithChildren<{ header: React.ReactNode; footer: React.ReactNode }>) {
 	return (
 		<aside className="sidebar-2">
 			<div className="sidebar-2-drag-area">
 				<div className="sidebar-drag-handle"></div>
 			</div>
 			<div className="sidebar-2-card">
-				<header className="sidebar-header">
+				{/* <header className="sidebar-header">
 					{header}
 					<hr className="collapse" />
-				</header>
+				</header> */}
 				<div className="sidebar-scroll-area">{children}</div>
-				<footer className="sidebar-footer">
+				{/* <footer className="sidebar-footer">
 					<hr className="collapse" />
 					{footer}
-				</footer>
+				</footer> */}
 			</div>
 		</aside>
 	)
 }
 
-function Main({ children }: PropsWithChildren) {
+function Main({ children }: React.PropsWithChildren) {
 	return <main className="main">{children}</main>
 }
 
@@ -131,7 +158,7 @@ export function App() {
 				<div>foo bar</div>
 				<div>foo bar</div>
 			</Main>
-			<Sidebar2
+			{/* <Sidebar2
 				// prettier-ignore
 				header={<div>foo bar</div>}
 				footer={<div>foo bar</div>}
@@ -141,7 +168,7 @@ export function App() {
 						<div>foo bar</div>
 					</React.Fragment>
 				))}
-			</Sidebar2>
+			</Sidebar2> */}
 		</>
 	)
 }
