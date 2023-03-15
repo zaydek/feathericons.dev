168094f78470a8cd77034a3a9f1b03355e24c4db
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2023-03-14 17:35:38 -0700

    Clean up typography, start writing new icon cache

--- src/providers/use-query-icons.ts
+++ src/providers/use-query-icons.ts
@@ -4,63 +4,70 @@ import { IconComponent } from "@/lib"
 import { WkPaymentsValue } from "@/providers"
 import { useQuery } from "@tanstack/react-query"
 
-//// type ImportPath =
-//// 	| "@icons/feather/tsx"
-//// 	| "@icons/wolfkit/brands/mono/tsx"
-//// 	| "@icons/wolfkit/brands/original/tsx"
-//// 	| "@icons/wolfkit/payments/mono/tsx"
-//// 	| "@icons/wolfkit/payments/mono-filled/tsx"
-//// 	| "@icons/wolfkit/payments/original/tsx"
-//// 	| "@icons/wolfkit/payments/original-filled/tsx"
-////
-//// export function createCache() {
-//// 	const cache = new Map<ImportPath, Record<string, Icon>>()
-//// 	async function read(path: ImportPath): Promise<Record<string, Icon>> {
-//// 		if (cache.has(path)) {
-//// 			return new Promise(resolve => resolve(cache.get(path)!))
-//// 		}
-//// 		switch (path) {
-//// 			case "@icons/feather/tsx":
-//// 				return import("@icons/feather/tsx").then(mod => {
-//// 					cache.set(path, mod)
-//// 					return mod
-//// 				})
-//// 			case "@icons/wolfkit/brands/mono/tsx":
-//// 				return import("@icons/wolfkit/brands/mono/tsx").then(mod => {
-//// 					cache.set(path, mod)
-//// 					return mod
-//// 				})
-//// 			case "@icons/wolfkit/brands/original/tsx":
-//// 				return import("@icons/wolfkit/brands/original/tsx").then(mod => {
-//// 					cache.set(path, mod)
-//// 					return mod
-//// 				})
-//// 			case "@icons/wolfkit/payments/mono/tsx":
-//// 				return import("@icons/wolfkit/payments/mono/tsx").then(mod => {
-//// 					cache.set(path, mod)
-//// 					return mod
-//// 				})
-//// 			case "@icons/wolfkit/payments/mono-filled/tsx":
-//// 				return import("@icons/wolfkit/payments/mono-filled/tsx").then(mod => {
-//// 					cache.set(path, mod)
-//// 					return mod
-//// 				})
-//// 			case "@icons/wolfkit/payments/original/tsx":
-//// 				return import("@icons/wolfkit/payments/original/tsx").then(mod => {
-//// 					cache.set(path, mod)
-//// 					return mod
-//// 				})
-//// 			case "@icons/wolfkit/payments/original-filled/tsx":
-//// 				return import("@icons/wolfkit/payments/original-filled/tsx").then(mod => {
-//// 					cache.set(path, mod)
-//// 					return mod
-//// 				})
-//// 		}
-//// 	}
-//// 	return read
+type ImportPath =
+	| "@icons/feather/tsx"
+	| "@icons/wk/brands/mono/tsx"
+	| "@icons/wk/brands/original/tsx"
+	| "@icons/wk/payments/mono/tsx"
+	| "@icons/wk/payments/mono-filled/tsx"
+	| "@icons/wk/payments/original/tsx"
+	| "@icons/wk/payments/original-filled/tsx"
+
+export function createCache() {
+	const cache = new Map<ImportPath, Record<string, IconComponent>>()
+	async function read(...args: ImportPath[]) {
+		let cached = true
+		const chain: Promise<Record<string, IconComponent>>[] = []
+		for (const arg of args) {
+			if (cache.has(arg)) {
+				chain.push(new Promise(resolve => resolve(cache.get(arg)!)))
+				continue
+			}
+			cached = false
+			switch (arg) {
+				case "@icons/feather/tsx":
+					chain.push(import("@icons/feather/tsx"))
+					break
+				case "@icons/wk/brands/mono/tsx":
+					chain.push(import("@icons/wk/brands/mono/tsx"))
+					break
+				case "@icons/wk/brands/original/tsx":
+					chain.push(import("@icons/wk/brands/original/tsx"))
+					break
+				case "@icons/wk/payments/mono/tsx":
+					chain.push(import("@icons/wk/payments/mono/tsx"))
+					break
+				case "@icons/wk/payments/mono-filled/tsx":
+					chain.push(import("@icons/wk/payments/mono-filled/tsx"))
+					break
+				case "@icons/wk/payments/original/tsx":
+					chain.push(import("@icons/wk/payments/original/tsx"))
+					break
+				case "@icons/wk/payments/original-filled/tsx":
+					chain.push(import("@icons/wk/payments/original-filled/tsx"))
+					break
+			}
+		}
+		const done = await Promise.all(chain)
+		for (const [index, path] of args.entries()) {
+			cache.set(path, done[index])
+		}
+		const normalized = done.reduce<[string, IconComponent][]>((acc, sum) => {
+			for (const entry of Object.entries(sum)) {
+				acc.push(entry)
+			}
+			return acc
+		}, [])
+		return [cached, normalized] as const
+	}
+	return { read }
+}
+
+//// async function run() {
+//// 	const { read } = createCache()
+//// 	console.log(await read("@icons/feather/tsx"))
 //// }
-////
-//// const read = createCache()
+//// run()
 
 export async function fetchIconsets(
 	iconsets: {
