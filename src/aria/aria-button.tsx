7b019c57b600a67078f640c9e3f5a88d1cc2431b
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2023-03-15 20:05:48 -0700

    Work on improved AriaButton component

--- src/aria/aria-button.tsx
+++ src/aria/aria-button.tsx
@@ -1,17 +1,54 @@
-import { HTMLAttributes } from "react"
+import React from "react"
 
-export function AriaButton({ children, ...props }: HTMLAttributes<HTMLDivElement>) {
+// prettier-ignore
+export type AriaButtonProps = React.PropsWithChildren<{
+	onPress: (
+		event:
+			| React.MouseEvent<HTMLDivElement>
+			| React.KeyboardEvent<HTMLDivElement>
+		) => void
+	label:     string
+	disabled?: boolean
+}>
+
+export function AriaButton({
+	// prettier-ignore
+	onPress,
+	label,
+	disabled = false,
+	children,
+}: AriaButtonProps) {
+	const [keyboardActive, setKeyboardActive] = React.useState(false)
+
 	return (
 		<div
-			onKeyDown={e => {
-				if (e.key === " ") {
+			// A11y
+			role="button"
+			aria-label={label}
+			aria-disabled={disabled}
+			tabIndex={disabled ? -1 : 0}
+			// /A11y
+			onClick={(e: React.MouseEvent<HTMLDivElement>) => {
+				if (disabled) return
+				onPress(e)
+			}}
+			onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => {
+				if (disabled) return
+				if (e.key === " " || e.key === "Enter") {
 					e.preventDefault()
-					e.currentTarget.click()
+					onPress(e)
+					setKeyboardActive(true)
 				}
 			}}
-			role="button"
-			tabIndex={0}
-			{...props}
+			onKeyUp={(e: React.KeyboardEvent<HTMLDivElement>) => {
+				if (disabled) return
+				if (e.key === " " || e.key === "Enter") {
+					e.preventDefault()
+					setKeyboardActive(false)
+				}
+			}}
+			// Adds support for keyboard :hover:focus
+			data-keyboard-active={keyboardActive}
 		>
 			{children}
 		</div>
