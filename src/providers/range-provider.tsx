82fc03513d601b35ff8d556d03107b4205a77cdd
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2023-03-16 20:28:05 -0700

    Use Sass variables for colors, colors are still bad though

--- src/providers/range-provider.tsx
+++ src/providers/range-provider.tsx
@@ -3,9 +3,9 @@ import React from "react"
 import { clampValue, useParam } from "@/lib"
 
 export const SIZE_MIN       = 16 // prettier-ignore
-export const SIZE_MAX       = 48 // prettier-ignore
+export const SIZE_MAX       = 32 // prettier-ignore
 export const SIZE_STEP      =  1 // prettier-ignore
-export const SIZE_DEFAULT   = 32 // prettier-ignore
+export const SIZE_DEFAULT   = 24 // prettier-ignore
 
 export const STROKE_MIN     =  0.5 // prettier-ignore
 export const STROKE_MAX     =  3.5 // prettier-ignore
